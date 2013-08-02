3162d504b7ddfd06239d8c6b2fde56a90cf03e88
Michel Ferrero <mferrero@cpht.polytechnique.fr>
2013-08-02 10:13:46 +0200

    Add some more direct accesses in pytriqs/operators
      modified:   pytriqs/operators/__init__.py

--- pytriqs/operators/__init__.py
+++ pytriqs/operators/__init__.py
@@ -20,5 +20,7 @@
 #
 ################################################################################
 
-from operators import Operator, C, Cdag, N, state, all_pure_states, C_list, number_of_C, commutator, anti_commutator, operator_id, extend_function_on_fundamentals
+from operators import Operator, C, Cdag, N, state, all_pure_states, C_list, number_of_C, commutator, anti_commutator, operator_id, extend_function_on_fundamentals, C_list_names, complete_op_list_with_fundamentals, transcribe_op_list_for_C
+
+__all__ = ['Operator', 'C', 'Cdag', 'N', 'state', 'all_pure_states', 'C_list', 'number_of_C', 'commutator', 'anti_commutator', 'operator_id', 'extend_function_on_fundamentals', 'C_list_names', 'complete_op_list_with_fundamentals', 'transcribe_op_list_for_C']
 
