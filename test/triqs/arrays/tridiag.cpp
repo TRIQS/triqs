1a90d441cb85af3e45e8d7f4376aa4e5d89192d8
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-09-03 21:11:49 +0200

    tridiag_worker: support for complex Hermitian matrices
    Real/complex implementation is switched with a bool template parameter
    of tridiag_worker. Set to real by default.

--- test/triqs/arrays/tridiag.cpp
+++ test/triqs/arrays/tridiag.cpp
@@ -26,21 +26,24 @@ using namespace triqs;
 
 int main(int argc, char **argv) {
 
+ std::cerr << "Real matrix" << std::endl;
+ {
  arrays::vector<double> D (5), DD(4);
  for (size_t i=0; i<5; ++i) {D(i) = 3.2*i+2.3;}
  for (size_t i=0; i<4; ++i) {DD(i) = 2.4*(i+1.82) + 0.78;} 
 
  auto R = range(0,5);
 
- blas::tridiag_worker w(10);
+ blas::tridiag_worker<false> w(10);
  w(D,DD);
  std::cerr<<" values = "<< w.values()<<std::endl;
  std::cerr<<" vectors = "<< w.vectors()(R,R)<<std::endl;
 
- //check 
+ //check
  matrix<double> A(5,5); A()=0;
  for (size_t i=0; i<5; ++i) A(i,i) = D(i);
- for (size_t i=0; i<4; ++i) { A(i,i+1) = DD(i); A(i+1,i) = DD(i);} 
+ for (size_t i=0; i<4; ++i) { A(i,i+1) = DD(i); A(i+1,i) = DD(i);}
+ auto AA = A; // A will be destroyed by linalg::eigenelements_worker
 
  std::cerr<<"A = "<<A<<std::endl;
  linalg::eigenelements_worker< matrix_view <double>, true> w2 (A());
@@ -49,7 +52,48 @@ int main(int argc, char **argv) {
  std::cerr<<" check vectors = "<< w2.vectors()<<std::endl;
 
  assert_all_close(w.vectors()(R,R), w2.vectors(), 1.e-10);
+ arrays::vector<double> zero(5); zero() = 0;
+ for(int n=0; n<5; ++n){
+    arrays::vector<double> diff = (AA - w.values()(n))*w.vectors()(n,R);
+    assert_all_close(diff,zero,1.e-10);
+ }
+ }
+
+ std::cerr << std::endl << "Complex matrix" << std::endl;
+ {
+ std::complex<double> I(0,1.0);
+ arrays::vector<double> D (5);
+ arrays::vector<std::complex<double>> DD(4);
+ for (size_t i=0; i<5; ++i) {D(i) = 3.2*i+2.3;}
+ for (size_t i=0; i<4; ++i) {DD(i) = 2.4*(i+1.82) + 0.78*I;}
+ DD(1) = 0;
+
+ auto R = range(0,5);
+
+ blas::tridiag_worker<true> w(10);
+ w(D,DD);
+ std::cerr<<" values = "<< w.values()<<std::endl;
+ std::cerr<<" vectors = "<< w.vectors()(R,R)<<std::endl;
+
+ //check
+ matrix<std::complex<double>> A(5,5); A()=0;
+ for (size_t i=0; i<5; ++i) A(i,i) = D(i);
+ for (size_t i=0; i<4; ++i) {A(i,i+1) = DD(i); A(i+1,i) = conj(DD(i));}
+ auto AA = A;   // A will be destroyed by linalg::eigenelements_worker
+
+ std::cerr<<"A = "<<A<<std::endl;
+ linalg::eigenelements_worker<matrix_view<std::complex<double>>, true> w2 (A());
+ w2.invoke();
+ std::cerr<<" check values = "<< w2.values()<<std::endl;
+ std::cerr<<" check vectors = "<< w2.vectors()<<std::endl;
+
  assert_all_close(array_view<double,1>(w.values()), w2.values(), 1.e-10);
+ arrays::vector<std::complex<double>> zero(5); zero() = 0;
+ for(int n=0; n<5; ++n){
+    arrays::vector<std::complex<double>> diff = (AA - w.values()(n))*w.vectors()(n,R);
+    assert_all_close(diff,zero,1.e-10);
+ }
+ }
 
  return 0;
 }
--- triqs/arrays/blas_lapack/stev.hpp
+++ triqs/arrays/blas_lapack/stev.hpp
@@ -36,7 +36,9 @@ namespace triqs { namespace arrays { namespace blas {
   }
  }
 
- struct tridiag_worker { 
+ template<bool Complex = false> struct tridiag_worker {};
+
+ template<> struct tridiag_worker<false> {
   arrays::vector<double> D,E,W;
   matrix<double> Z;
   size_t s;
@@ -54,6 +56,39 @@ namespace triqs { namespace arrays { namespace blas {
    }
   arrays::vector_view<double> values() const { return D(range(0,s));}
   matrix_view<double> vectors() const { return Z(range(0,s),range(0,s));}
+ };
+
+ template<> struct tridiag_worker<true> {
+  arrays::vector<double> D,E,W;
+  matrix<double> Z;
+  arrays::vector<std::complex<double>> U; // similarity transformation
+  matrix<std::complex<double>> V;         // transformed eigenvectors
+  size_t s;
+  tridiag_worker (size_t n){ s=0; resize(n);}
+  void resize(size_t n) { if (s<n) {D.resize(n); E.resize(n); W.resize(2*n+1); Z.resize(n,n); U.resize(n); V.resize(n,n);} s = n; }
+  template<typename VTd, typename VTe>
+   void operator()(VTd const & d, VTe const & e /* superdiagonal */) {
+    if (e.size() != d.size() -1) TRIQS_RUNTIME_ERROR << " Error in tridiagonal matrix diagonalization size mismatch";
+    resize(d.size());
+    D(range(0,s)) = d;
+    // construct transformed off-diagonal elements
+    U(0) = 1.0;
+    for(size_t n=0; n<s-1; ++n){
+     double abs_e = abs(e(n));
+     U(n+1) = U(n)*(abs_e<1e-10 ? 1.0 : conj(e(n))/abs_e);
+     E(n) = (e(n)*conj(U(n))*U(n+1)).real();
+    }
+    int info;
+    f77::stev ('V',d.size(),D.data_start(),E.data_start(), Z.data_start(), Z.shape(0),W.data_start(), info);
+    if (info !=0) TRIQS_RUNTIME_ERROR << " Error in tridiagonal matrix diagonalization "<< info;
+
+    // Back-transform the eigenvectors
+    for(size_t n1=0; n1<s; ++n1)
+     for(size_t n2=0; n2<s; ++n2)
+      V(n1,n2) = Z(n1,n2)*conj(U(n1))*U(n2);
+   }
+  arrays::vector_view<double> values() const { return D(range(0,s));}
+  matrix_view<std::complex<double>> vectors() const { return V(range(0,s),range(0,s));}
  };
 
 }}}// namespace
