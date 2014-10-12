dddae1985397f5e83aa67afb5792e582497ef393
Olivier Parcollet <olivier.parcollet@cea.fr>
2014-10-12 19:32:45 +0200

    Clean old boost and old mc_tools code (v1)

--- test/triqs/arrays/non_numeric.cpp
+++ test/triqs/arrays/non_numeric.cpp
@@ -20,71 +20,51 @@
  *
  ******************************************************************************/
 #include "./common.hpp"
-#include <triqs/arrays/array.hpp>
-#include <triqs/arrays/matrix.hpp>
-#include <iostream>
-
-using std::cout; using std::endl;
 using namespace triqs::arrays;
- 
-#include <boost/function.hpp>
-#include <boost/lambda/lambda.hpp>
-#include <boost/lambda/bind.hpp>
-
-namespace BLL=boost::lambda;
 
 struct S {
- double x,y;
- int i;
- S(): x(0), y(0), i(0) {} 
- S(double x0, double y0, int i0): x(x0), y(y0), i(i0) {} 
+ double x = 0, y = 0;
+ int i = 0;
 };
 
-double my_fun(double x, double y) { return x+y;}
-
-std::ostream & operator << (std::ostream & out, S const & s) { return out<<"("<<s.x<<" "<<s.y<<" "<<s.i<<")";}
-//std::ostream & operator << (std::ostream & out, S const & s) { return out<<std::make_tuple(s.x,s.y,s.i);}
+std::ostream &operator<<(std::ostream &out, S const &s) { return out << "(" << s.x << " " << s.y << " " << s.i << ")"; }
 
 int main(int argc, char **argv) {
- 
- 
 
+ array<S, 2> A(2, 2);
 
- array<S,2> A(2,2);
+ S s0{1.0, 2.0, 3};
+ int p = 0;
+ for (int i = 0; i < 2; ++i)
+  for (int j = 0; j < 2; ++j, ++p) A(i, j) = S{double(i), double(j), p};
 
- S s0(1,2,3);
- int p=0;
- for (int i =0; i<2; ++i)
-  for (int j=0; j<2; ++j,++p) 
-       A(i,j) = S(i,j,p);
-
- std::cout<<"  A "<<A<<A(range(),0)<<std::endl;
+ std::cout << "  A " << A << A(range(), 0) << std::endl;
 
  A() = s0;
- std::cout<<"  A "<<A<<A(range(),0)<<std::endl;
+ std::cout << "  A " << A << A(range(), 0) << std::endl;
 
- array< array<double,1>, 2> AA(2,2);
+ array<array<double, 1>, 2> AA(2, 2);
 
- array<double,1> A0(2); 
- for (int i =0; i<2; ++i)
-     A0(i) = i; 
+ array<double, 1> A0(2);
+ for (int i = 0; i < 2; ++i) A0(i) = i;
 
- AA()=A0;
+ AA() = A0;
 
- std::cout<<"  AA "<<AA<<std::endl;
- 
- std::cout<<AA(0,0)<<std::endl;
+ std::cout << "  AA " << AA << std::endl;
+
+ std::cout << AA(0, 0) << std::endl;
 
  // even more  : a matrix of functions !
- matrix< boost::function<double(double) > > F (2,2);
+ matrix<std::function<double(double)>> F(2, 2);
 
-for (int i =0; i<2; ++i)
-  for (int j=0; j<2; ++j,++p) 
-    F(i,j) = BLL::bind(&my_fun, BLL::_1, i+j); 
+ for (int i = 0; i < 2; ++i)
+  for (int j = 0; j < 2; ++j) {
+   auto s = i + j;
+   F(i, j) = [s](int i) { return i + s; };
+  }
 
-for (int i =0; i<2; ++i)
-  for (int j=0; j<2; ++j,++p) 
-    std::cout<<F(i,j)(0)<<std::endl; 
+ for (int i = 0; i < 2; ++i)
+  for (int j = 0; j < 2; ++j) std::cout << F(i, j)(0) << std::endl;
 
  return 0;
 }
