b2c32b0daa10f96d1f43336924d49b47517bfebe
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-12-07 22:30:12 +0100

    [doc] 2 small fixes in block GF examples.

--- doc/reference/gfs/py/block/green_imfreq.py
+++ doc/reference/gfs/py/block/green_imfreq.py
@@ -5,7 +5,7 @@ g = GfImFreq(indices = ['eg1','eg2'], beta = 50, n_points = 1000, name = "egBloc
 g['eg1','eg1'] = SemiCircular(half_bandwidth = 1)
 g['eg2','eg2'] = SemiCircular(half_bandwidth = 2)
 
-from pytriqs.plot.mpl_interface import oplot
+from pytriqs.plot.mpl_interface import oplot, plt
 oplot(g, '-o', x_window = (0,10))
 plt.ylim(-2,1)
 
