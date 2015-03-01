a8f683282c0f88e76a0a8fd726c38b8ee9fd3864
Igor Krivenko <igor.s.krivenko@gmail.com>
2015-03-01 22:40:04 +0100

    [hilbert_space] Major optimization of space_partition::merge_subspaces()

--- triqs/hilbert_space/space_partition.hpp
+++ triqs/hilbert_space/space_partition.hpp
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ *
+ * TRIQS: a Toolbox for Research in Interacting Quantum Systems
+ *
+ * Copyright (C) 2015 by I. Krivenko, M. Ferrero
+ *
+ * TRIQS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TRIQS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
 #pragma once
 
 #include <set>
@@ -5,6 +26,7 @@
 #include <utility>
 #include <triqs/utility/draft/numeric_ops.hpp>
 #include <boost/pending/disjoint_sets.hpp>
+#include <boost/timer/timer.hpp>
 
 namespace triqs {
 namespace hilbert_space {
@@ -27,6 +49,7 @@ template <typename StateType, typename OperatorType> class space_partition {
 
  space_partition(state_t const& st, operator_t const& H, bool store_matrix_elements = true)
     : subspaces(st.size()), tmp_state(make_zero_state(st)) {
+  boost::timer::auto_cpu_timer timer("space_partition::space_partition(): %ws wall, %us user + %ss system = %ts CPU (%p%)\n");
   auto size = tmp_state.size();
 
   // Iteration over all initial basis states
@@ -51,68 +74,79 @@ template <typename StateType, typename OperatorType> class space_partition {
 
  space_partition(space_partition const&) = default;
 
- // Algorithm by Michel Ferrero
- // FIXME WARNING: Redundant implementation, must be revisited later
  std::pair<matrix_element_map_t, matrix_element_map_t> merge_subspaces(operator_t const& Cd, operator_t const& C,
                                                                        bool store_matrix_elements = true) {
 
+  boost::timer::auto_cpu_timer timer("space_partition::merge_subspaces(): %ws wall, %us user + %ss system = %ts CPU (%p%)\n");
+
   matrix_element_map_t Cd_elements, C_elements;
+  std::multimap<index_t,index_t> Cd_connections, C_connections;
 
-  std::set<index_t> initial_basis_states;
-  for (index_t i = 0; i < tmp_state.size(); ++i) initial_basis_states.insert(i);
+  auto size = tmp_state.size();
 
-  do {
-   bool merge_occured;
-   index_t i = *initial_basis_states.cbegin();
+  // Fill connection multimaps
+  for (index_t i = 0; i < size; ++i) {
+   tmp_state(i) = amplitude_t(1.0);
+   auto i_subspace = subspaces.find_set(i);
 
-   auto apply_and_merge = [&](operator_t const& op, matrix_element_map_t& me, bool exclude_from_ibs) {
-    index_t final_subspace = -1;
-
-    // Iteration over all initial basis states
-    for (index_t n = 0; n < tmp_state.size(); ++n) {
-     // Is n in initial_subspace?
-     if (subspaces.find_set(i) != subspaces.find_set(n)) continue;
-
-     if (exclude_from_ibs && initial_basis_states.count(n) != 0) initial_basis_states.erase(n);
-
-     tmp_state(n) = amplitude_t(1.);
-     state_t final_state = op(tmp_state);
-
-     foreach(final_state, [&](index_t f, amplitude_t amplitude) {
-      if (triqs::utility::is_zero(amplitude)) return;
-
-      auto f_subspace = subspaces.find_set(f);
-      if (final_subspace == -1)
-       final_subspace = f_subspace;
-      else {
-       if (final_subspace != f_subspace) {
-        subspaces.link(final_subspace, f_subspace);
-        merge_occured = true;
-       }
-      }
-
-      if (store_matrix_elements) me.insert(std::make_pair(std::make_pair(n, f), amplitude));
-     });
-
-     tmp_state(n) = amplitude_t(0.);
-    }
-    i = final_subspace;
+   auto fill_conn = [this,i,i_subspace,store_matrix_elements]
+                    (operator_t const& op, std::multimap<index_t,index_t> & conn, matrix_element_map_t & elem) {
+    state_t final_state = op(tmp_state);
+    // Iterate over non-zero final amplitudes
+    foreach(final_state, [&](index_t f, amplitude_t amplitude) {
+     if (triqs::utility::is_zero(amplitude)) return;
+     auto f_subspace = subspaces.find_set(f);
+     conn.insert({i_subspace,f_subspace});
+     if (store_matrix_elements) elem[{i, f}] = amplitude;
+    });
    };
 
-   do {
-    merge_occured = false;
+   fill_conn(Cd,Cd_connections,Cd_elements);
+   fill_conn(C,C_connections,C_elements);
 
-    // Apply op1 to i subspace and merge final subspaces
-    // All used initial states will be excluded from initial_basis_states
-    apply_and_merge(Cd, Cd_elements, true);
-    if (i == -1) break; // No final subspace, just 0
-    // Apply op2 to i subspace and merge final subspaces
-    apply_and_merge(C, C_elements, false);
-    if (i == -1) break; // No final subspace, just 0
+   tmp_state(i) = amplitude_t(0.);
+  }
 
-   } while (merge_occured); // If there were merges, repeat application once more
+  // 'Zigzag' traversal algorithm
+  while(!Cd_connections.empty()) {
 
-  } while (initial_basis_states.size() != 0);
+   // Take one C^+ - connection
+   // C^+|lower_subspace> = |upper_subspace>
+   index_t lower_subspace, upper_subspace;
+   std::tie(lower_subspace,upper_subspace) = *std::begin(Cd_connections);
+
+   // - Reveals all subspaces reachable from lower_subspace by application of
+   //   a 'zigzag' product C^+ C C^+ C C^+ ... of any length.
+   // - Removes all visited connections from Cd_connections/C_connections.
+   // - Merges lower_subspace with all subspaces generated from lower_subspace by application of (C C^+)^(2*n).
+   // - Merges upper_subspace with all subspaces generated from upper_subspace by application of (C^+ C)^(2*n).
+   std::function<void(index_t,index_t,bool)> zigzag_traversal =
+    [this,lower_subspace,upper_subspace,&Cd_connections,&C_connections,&zigzag_traversal]
+    (index_t i_subspace,        // find all connections starting from i_subspace
+     index_t prev_i_subspace,   // this was 'i_subspace' of the parent zigzag_traversal call
+     bool upwards               // if true, C^+ connection, otherwise C connection
+    ){
+    std::multimap<index_t,index_t>::iterator it;
+    while((it = (upwards ? Cd_connections : C_connections).find(i_subspace)) !=
+                (upwards ? Cd_connections : C_connections).end()) {
+
+     auto f_subspace = it->second;
+     (upwards ? Cd_connections : C_connections).erase(it);
+     // Have we returned to the same subspace in two successive calls to zigzag_traversal?
+     // If so, stop traversing this branch.
+     if(f_subspace == prev_i_subspace) continue;
+
+     if(upwards) subspaces.link(f_subspace, upper_subspace);
+     else        subspaces.link(f_subspace, lower_subspace);
+
+     // Recursively apply to all found f_subspace's with a 'flipped' direction
+     zigzag_traversal(f_subspace,i_subspace,!upwards);
+    }
+   };
+
+   // Apply to all C^+ connections starting from lower_subspace
+   zigzag_traversal(lower_subspace,lower_subspace,true);
+  }
 
   _update_index();
 
