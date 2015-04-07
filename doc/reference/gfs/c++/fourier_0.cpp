3cdd8e549e5ae8e1dcd4316d6fa5711dc89fad00
tayral <thomas.ayral@cpht.polytechnique.fr>
2015-04-07 11:56:22 +0200

    [doc] Fixing reference/gfs/c++/fourier_0

--- doc/reference/gfs/c++/fourier_0.cpp
+++ doc/reference/gfs/c++/fourier_0.cpp
@@ -4,7 +4,7 @@ int main() {
  double beta = 1, a = 1;
  int N = 10000;
  auto gw = gf<imfreq>{{beta, Fermion, N/2}, {1, 1}};
- auto gt = gf<imtime>{{beta, Fermion, N}, {1, 1}};
+ auto gt = gf<imtime>{{beta, Fermion, N+1}, {1, 1}};
 
  triqs::clef::placeholder<0> om_;
  gw(om_) << 1 / (om_ - a);
