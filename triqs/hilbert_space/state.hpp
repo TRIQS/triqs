fa0105ba9bb0e316ed904ef3147be01444f37ced
Igor Krivenko <igor.s.krivenko@gmail.com>
2015-06-13 00:47:10 +0200

    [hilbert_space] Fix for function project(state,hilbert_space)
    * A workaround for a bug in Clang 3.4 was itself buggy.

--- triqs/hilbert_space/state.hpp
+++ triqs/hilbert_space/state.hpp
@@ -265,7 +265,7 @@ template<typename TargetState, typename OriginalState>
 TargetState project(OriginalState const& psi, hilbert_space const& proj_hs) {
  TargetState proj_psi(proj_hs);
  auto const& hs = psi.get_hilbert();
- auto f = __lambda1<TargetState,hilbert_space> {proj_psi, hs};
+ auto f = __lambda1<TargetState,decltype(hs)> {proj_psi, hs};
  foreach(psi, f);
  return proj_psi;
 }
@@ -274,7 +274,7 @@ template<typename TargetState, typename OriginalState>
 TargetState project(OriginalState const& psi, sub_hilbert_space const& proj_hs) {
  TargetState proj_psi(proj_hs);
  auto const& hs = psi.get_hilbert();
- auto f = __lambda2<TargetState,sub_hilbert_space,hilbert_space> {proj_psi, proj_hs, hs};
+ auto f = __lambda2<TargetState,sub_hilbert_space,decltype(hs)> {proj_psi, proj_hs, hs};
  foreach(psi,f);
  return proj_psi;
 }
