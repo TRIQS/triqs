d3943fbb9705b7ee423514ad0313abac87d86bf6
Olivier Parcollet <olivier.parcollet@cea.fr>
2015-08-01 16:46:47 +0200

    [cpp2py] Fix the libclang detection for Yosemite
    - Use the same call to clang as for linux to detect the proper includes.

--- cmake/FindLibClang.cmake
+++ cmake/FindLibClang.cmake
@@ -8,6 +8,17 @@
 # 
 
 # First find libclang if it is not provided
+
+if( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+ 
+  set(TRIQS_LIBCLANG_LOCATION "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/libclang.dylib" CACHE STRING "Location of the libclang library")
+  set(TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS "" CACHE STRING "Additional flags to be passed to libclang when parsing with clang")
+
+  set(CLANG_COMPILER "clang++")
+  set(CLANG_OPT  -std=c++1y -stdlib=libc++ )
+
+else()
+
 SET(TRIAL_LIBRARY_PATHS
  /usr/lib 
  /usr/lib/x86_64-linux-gnu
@@ -23,7 +34,8 @@ endif()
 if (NOT TRIQS_LIBCLANG_LOCATION)
  MESSAGE(STATUS "Can not find libclang")
 endif()
- 
+
+
 SET(TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS "${TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS}")
 #SET(TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS "-DTRIQS_ADD_MAX_ALIGN_T_WORKAROUND ${TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS}")
 
@@ -35,14 +47,20 @@ else()
  SET(TRIAL_CLANG_PATHS
  /usr/bin 
  )
-FIND_PROGRAM(CLANG_COMPILER names clang++ clang++-3.5 PATHS ${TRIAL_CLANG_PATHS} DOC "Clang compiler (for libclang option)")
+FIND_PROGRAM(CLANG_COMPILER names clang++ clang++-3.7 clang++-3.6 clang++-3.5 PATHS ${TRIAL_CLANG_PATHS} DOC "Clang compiler (for libclang option)")
 endif(${CMAKE_COMPILER_IS_CLANG})
 endif()
 
 if (NOT CLANG_COMPILER)
  MESSAGE(STATUS "Can not find the Clang compiler, hence can not find the option for libclang")
-else()
-SET(LIBCLANG_FLAGS_DETECTION_COMMAND "${CLANG_COMPILER}" -E -x c++ -v -)
+endif()
+
+ set(CLANG_OPT -std=c++1y )
+
+endif( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+
+
+SET(LIBCLANG_FLAGS_DETECTION_COMMAND "${CLANG_COMPILER}" -E -x c++ ${CLANG_OPT} -v -)
 EXECUTE_PROCESS(COMMAND ${LIBCLANG_FLAGS_DETECTION_COMMAND}
                 INPUT_FILE /dev/null
                 ERROR_VARIABLE _compiler_output OUTPUT_QUIET)
@@ -55,7 +73,6 @@ FOREACH(include_path IN ITEMS ${CMAKE_MATCH_1})
  STRING(STRIP ${include_path} include_path)
  SET(TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS "${TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS} -I${include_path}")
 ENDFOREACH(include_path IN ITEMS ${CMAKE_MATCH_1})
-endif()
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibClang DEFAULT_MSG TRIQS_LIBCLANG_LOCATION TRIQS_LIBCLANG_CXX_ADDITIONAL_FLAGS)
 
