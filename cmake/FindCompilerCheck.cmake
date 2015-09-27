4be6761cd19ad0daabe2e60065b0cf8644d49d4f
Olivier Parcollet <olivier.parcollet@cea.fr>
2015-09-27 14:48:35 +0200

    [cmake] Rm compiler version check on OS X.
    Broken because of new Xcode release, and not very useful on OS X anyway.
    Default clang compiles TRIQS for quite some time.

--- cmake/FindCompilerCheck.cmake
+++ cmake/FindCompilerCheck.cmake
@@ -72,11 +72,13 @@ endif ()
 
 MESSAGE( STATUS "Compiler is ${compiler_name} with version ${compiler_version}")
 
-# Check version 
+# Check version. Does not work on OS X ... 
+IF(NOT ${CMAKE_SYSTEM_NAME}  MATCHES "Darwin")
 if(compiler_version VERSION_LESS ${compiler_version_min} )
  set(line_of_star "\n************************** FATAL ERROR ************************************\n")
  MESSAGE( FATAL_ERROR "${line_of_star}You are using the ${compiler_name} compiler but your compiler is too old :\n TRIQS requires version >= ${compiler_version_min} while you have ${compiler_version}\n  ${line_of_star}")
 endif(compiler_version VERSION_LESS ${compiler_version_min} )
+ENDIF()
 
 # Now add some definitions ...
 
