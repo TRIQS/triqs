9ac4fd3f638a83da682f586579467a0298b52a4b
Igor Krivenko <igor.s.krivenko@gmail.com>
2015-10-15 21:26:29 +0200

    [operators] Full-blown test for extract_h_dict and extract_U_dict2

--- test/triqs/operators/extractors.cpp
+++ test/triqs/operators/extractors.cpp
@@ -1,39 +1,105 @@
-#include <triqs/utility/first_include.hpp>
+#include "test_tools.hpp"
+
+#define TRIQS_ARRAYS_ENFORCE_BOUNDCHECK
+#include <vector>
+#include <triqs/operators/many_body_operator.hpp>
 #include <triqs/operators/util/extractors.hpp>
 
 using namespace triqs::operators;
 using namespace triqs::operators::util;
+using namespace triqs::arrays;
 
-int main(){
- auto gf_struct = gf_struct_t({{"up", {0}}, {"down", {0}}});
- auto H = 0.1* n("up",0)*n("down",0)+ 0.5*n("up",1)*n("down",0) ;
- auto U_dict = extract_U_dict2(H);
- try{
-  auto U_matrix = dict_to_matrix(U_dict, gf_struct);
- }
- catch(triqs::exception const & e) {
-  std::cout << "Error: " << e.what() << std::endl;
- }
+double U1 = 2.0, U2 = 2.2, U3 = 2.4;
+double eps1 = 1.0, eps2 = 1.1, eps3 = 1.2;
+double t12 = 0.4, t23 = 0.5, t31 = 0.6;
+double V12 = 1.8, V23 = 1.7, V31 = 1.6;
 
- auto H2 = 0.1* n("up",0)*n("down",0);
- auto U_dict2 = extract_U_dict2(H2);
- auto U_matrix2 = dict_to_matrix(U_dict2, gf_struct);
- std::cout << "U_matrix2 " << U_matrix2 << std::endl;
+using indices_t = typename op_t<double>::indices_t;
+auto gf_struct = gf_struct_t({{"up", {1,2,3}}, {"dn", {1,2,3}}});
+std::vector<indices_t> ind = {{"dn",1},{"dn",2},{"dn",3},{"up",1},{"up",2},{"up",3}};
 
- auto U_dict4 = extract_U_dict4(H2);
- auto U_matrix4 = dict_to_matrix(U_dict4, gf_struct);
- std::cout << "U_matrix4 " << U_matrix4 << std::endl;
+// Test extract_h_dict
+TEST(Extractors, extract_h_dict) {
+ auto h0 = eps1*(n("up",1)+n("dn",1)) +
+           eps2*(n("up",2)+n("dn",2)) +
+           eps3*(n("up",3)+n("dn",3));
+ auto hop = t12*(c_dag("up",1)*c("up",2) + c_dag("dn",1)*c("dn",2)) +
+            t23*(c_dag("up",2)*c("up",3) + c_dag("dn",2)*c("dn",3)) +
+            t31*(c_dag("up",3)*c("up",1) + c_dag("dn",3)*c("dn",1));
+ h0 += hop + dagger(hop);
 
- auto H3 = -0.5*(n(0) + n(1)) + n(1)*n(0) + c_dag(1)*c(1)*c(2)*c(0);
- try{
-  auto U_dict3 = extract_U_dict2(H3);
- }
- catch(triqs::exception const & e) {
-  std::cout << "Error: " << e.what() << std::endl;
- }
+ array<double,2> h_matrix_ref
+  {{eps1, t12,  t31,  .0,   .0,   .0  },
+   {t12,  eps2, t23,  .0,   .0,   .0  },
+   {t31,  t23,  eps3, .0,   .0,   .0  },
+   {.0,   .0,   .0,   eps1, t12,  t31 },
+   {.0,   .0,   .0,   t12,  eps2, t23 },
+   {.0,   .0,   .0,   t31,  t23,  eps3}};
 
- auto h_dict = extract_h_dict(-0.5*(n("up",0) + n("down",0)));
- std::cout << dict_to_matrix(h_dict, gf_struct) << std::endl;
+ h_dict_t<double> h_dict_ref;
+ for(int i=0; i<6; ++i)
+  for(int j=0; j<6; ++j){
+   auto val = h_matrix_ref(i,j);
+   if(val!=.0) h_dict_ref[std::make_tuple(ind[i],ind[j])] = val;
+  }
 
- return 0;
+ auto h_dict = extract_h_dict(h0);
+ EXPECT_EQ(h_dict_ref,h_dict);
+ EXPECT_CLOSE_ARRAY(h_matrix_ref,dict_to_matrix(h_dict,gf_struct));
+
+ auto nn = n("up",1)*n("dn",1);
+ EXPECT_THROW(extract_h_dict(nn),triqs::exception);
+ EXPECT_NO_THROW(extract_h_dict(nn,true));
+
+ auto cc = c("up",1)*c("dn",2);
+ EXPECT_THROW(extract_h_dict(cc),triqs::exception);
+ EXPECT_NO_THROW(extract_h_dict(cc,true));
+
+ auto wrong_index = n("up",1) + n("dn",5);
+ EXPECT_THROW(dict_to_matrix(extract_h_dict(wrong_index),gf_struct),triqs::exception);
 }
+
+TEST(Extractors, extract_U_dict2) {
+ auto h_int = U1*n("up",1)*n("dn",1) +
+              U2*n("up",2)*n("dn",2) +
+              U3*n("up",3)*n("dn",3);
+ auto N1 = n("up",1)+n("dn",1);
+ auto N2 = n("up",2)+n("dn",2);
+ auto N3 = n("up",3)+n("dn",3);
+ h_int += V12*N1*N2 + V23*N2*N3 + V31*N1*N3;
+
+ array<double,2> U_matrix2_ref
+  {{.0,   V12,  V31,  U1,   V12,  V31},
+   {V12,  .0,   V23,  V12,  U2,   V23},
+   {V31,  V23,  .0,   V31,  V23,  U3 },
+   {U1,   V12,  V31,  .0,   V12,  V31},
+   {V12,  U2,   V23,  V12,  .0,   V23},
+   {V31,  V23,  U3,   V31,  V23,  .0 }};
+
+ U_dict2_t<double> U_dict2_ref;
+ for(int i=0; i<6; ++i)
+  for(int j=0; j<6; ++j){
+   auto val = U_matrix2_ref(i,j);
+   if(val!=.0) U_dict2_ref[std::make_tuple(ind[i],ind[j])] = val;
+  }
+
+ auto U_dict2 = extract_U_dict2(h_int);
+ EXPECT_EQ(U_dict2_ref,U_dict2);
+ EXPECT_CLOSE_ARRAY(U_matrix2_ref,dict_to_matrix(U_dict2,gf_struct));
+
+ auto quadratic = n("up",1) + n("dn",1);
+ EXPECT_THROW(extract_U_dict2(quadratic),triqs::exception);
+ EXPECT_NO_THROW(extract_U_dict2(quadratic,true));
+
+ auto non_nn = c_dag("up",1)*c_dag("dn",1)*c("up",3)*c("dn",2);
+ EXPECT_THROW(extract_U_dict2(non_nn),triqs::exception);
+ EXPECT_NO_THROW(extract_U_dict2(non_nn,true));
+
+ auto wrong_index = n("up",4)*n("dn",2);
+ EXPECT_THROW(dict_to_matrix(extract_U_dict2(wrong_index),gf_struct),triqs::exception);
+}
+
+//TEST(Extractors, ExtractH4) {
+//}
+
+MAKE_MAIN;
