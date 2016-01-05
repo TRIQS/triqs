d4427778fc0b10a52fee11ed84975cb075bb6861
JaksaVucicevic <jaksa.vucicevic@gmail.com>
2016-01-05 10:12:08 +0100

    Fixed initialization. Passes tests.

--- triqs/det_manip/det_manip.hpp
+++ triqs/det_manip/det_manip.hpp
@@ -37,70 +37,24 @@ namespace triqs { namespace det_manip {
  namespace blas = arrays::blas; 
 
  struct complex_as_logphi {
-   complex_as_logphi(): log(1.0), phi(1.0) {};
+   complex_as_logphi(): log(0.0), phi(0.0) {};
    complex_as_logphi(double log_, double phi_): log(log_), phi(phi_) {};
    double log;
    double phi;
 
-   complex_as_logphi& operator=(std::complex<double> a) {
-     log=std::log(std::abs(a));
-     phi=std::arg(a);
-     return *this;
-   }
+   complex_as_logphi& operator=(std::complex<double> a);
+   complex_as_logphi& operator*=(const complex_as_logphi& a);
+   complex_as_logphi& operator*=(std::complex<double> a);
 
-   complex_as_logphi& operator*=(const complex_as_logphi& a) {
-     log+=a.log;
-     phi+=a.phi;
-     return *this;
-   }
-
-   complex_as_logphi& operator*=(std::complex<double> a) {
-     log+=std::log(std::abs(a));
-     phi+=std::arg(a);
-     return *this;
-   }
-
-   explicit operator double() const {
-     double a = std::exp(log);
-     if (not std::isfinite(a)) TRIQS_RUNTIME_ERROR << "det_manip::determinant is underflowing!";
-     //if ( (phi!=0.0) or (phi != PI) )
-     //  TRIQS_RUNTIME_ERROR << "det_manip casting insto double truncates the imaginary part!";
-     return a*real(std::exp(std::complex<double>(0.0,1.0)*phi));
-   }
-
-   explicit operator std::complex<double>() const {
-     double a = std::exp(log);
-     if (not std::isfinite(a)) TRIQS_RUNTIME_ERROR << "det_manip::determinant is underflowing!";
-     return a*std::exp(std::complex<double>(0.0,1.0)*phi);
-   }
-
-
+   explicit operator double() const ;
+   explicit operator std::complex<double>() const ;
  }; 
 
- complex_as_logphi operator*(complex_as_logphi a, const complex_as_logphi& b) {
-   a*=b; 
-   return a;
- }
-
- complex_as_logphi operator*(complex_as_logphi a, std::complex<double> b) {
-   a*=b;
-   return a;
- }
-
- complex_as_logphi operator*(std::complex<double> b, complex_as_logphi a) {
-   return a*b;
- }
-
- complex_as_logphi operator/(complex_as_logphi a, std::complex<double> b) {
-   b = 1.0/b; 
-   return a*b;
- }
-
- complex_as_logphi operator/(std::complex<double> b, complex_as_logphi a) {
-   a.log *= -1.0;
-   a.phi *= -1.0;
-   return b*a;
- }
+ complex_as_logphi operator*(complex_as_logphi a, const complex_as_logphi& b);
+ complex_as_logphi operator*(complex_as_logphi a, std::complex<double> b);
+ complex_as_logphi operator*(std::complex<double> b, complex_as_logphi a);
+ complex_as_logphi operator/(complex_as_logphi a, std::complex<double> b);
+ complex_as_logphi operator/(std::complex<double> b, complex_as_logphi a);
 
 
  /**
@@ -328,7 +282,7 @@ namespace triqs { namespace det_manip {
 
     /** det M of the current state of the matrix.  */
     value_type determinant() const {
-      return value_type(det);
+      return sign*value_type(det);
     }
 
     /** Returns M^{-1}(i,j) */
