TRIQS/triqs

commit dd09f78151b68d24b40557c8d02d534ed2004c5a

Port to Intel icc 16.0. h5:vector bug fix. Fix #279
- A few small details to fix for icc
- variant : a woraround an icc bug
- h5: vector. Bug fix.
- mostly include pbs for Python warning.

triqs/utility/variant.hpp
@@ -28,6 +28,12 @@
 #include <ostream>
 #include <algorithm>
 
+// a special workaround for gcc < 5.x !
+#if !defined __INTEL_COMPILER  && defined __GNUC__ && GCC_VERSION < 50100
+ #define WORKAROUND_GCC49
+ #warning "Compiling with workaround for gcc < 5.x"
+#endif
+
 // Forward-declarations for Boost.Serialization
 // They are neccessary along with ugly friend declarations in class variant,
 // if we want to keep the serialization code in a separate file
@@ -176,7 +182,7 @@ namespace utility {
   template <typename T, typename F> std14::result_of_t<F(T const &)> apply_c(F f) const {
    return f(*reinterpret_cast<T const *>(&data));
   }
-
+ 
   public:
   template <typename = std14::enable_if_t<std::is_default_constructible<bounded_type<0>>::value>> variant() : type_id(0) {
    ::new (&data) bounded_type<0>();
@@ -231,21 +237,10 @@ namespace utility {
 // Visitation
 // Return type of f(v) must be the same for any stored type, so we can use the
 // first type.
-#if GCC_VERSION > 50200
-  template <typename F> friend auto visit(F &&f, variant &v) {
-   using RType = std14::result_of_t<F(bounded_type<0> &)>;
-   constexpr static std::array<RType (variant::*)(F), n_bounded_types> table = {&variant::apply<Types, F>...};
-   return (v.*table[v.type_id])(std::forward<F>(f));
-  }
-
-  template <typename F> friend auto visit(F &&f, variant const &v) {
-   using RType = std14::result_of_t<F(bounded_type<0> const &)>;
-   constexpr static std::array<RType (variant::*)(F) const, n_bounded_types> table = {&variant::apply_c<Types, F>...};
-   return (v.*table[v.type_id])(std::forward<F>(f));
-  }
-#else
+#ifdef WORKAROUND_GCC49
+  // Implementation for gcc 4.9 only.
   // A bug in gcc 4.9 (59766) forbids to use the friend auto which is clearer
-  // than this
+  // than this. For icc, I can not leave it here, need to put it outside the class, which is ok for gcc 5.x and clang !
   template <typename F, typename RType = std14::result_of_t<F(bounded_type<0> &)>> friend RType visit(F &&f, variant &v) {
    constexpr static std::array<RType (variant::*)(F), n_bounded_types> table = {&variant::apply<Types, F>...};
    return (v.*table[v.type_id])(std::forward<F>(f));
@@ -257,6 +252,7 @@ namespace utility {
    return (v.*table[v.type_id])(std::forward<F>(f));
   };
 #endif
+
 
   // Comparisons
   bool operator==(variant const &other) const {
@@ -285,6 +281,31 @@ namespace utility {
    visit([&os](auto const &x) { os << x; }, v);
    return os;
   }
+#ifndef WORKAROUND_GCC49
+  template <typename F, typename ... T> friend auto visit(F &&f, variant<T...> &v);
+  template <typename F, typename ... T> friend auto visit(F &&f, variant<T...> const &v);
+#endif
  };
+
+#ifndef WORKAROUND_GCC49
+// Visitation
+// Return type of f(v) must be the same for any stored type, so we can use the
+// first type
+  template<typename T0, typename ...T> struct _first_type{ using type  = T0;};
+
+  template <typename F, typename ... T> auto visit(F &&f, variant<T...> &v) {
+   using RType = std14::result_of_t<F(typename _first_type<T...>::type &)>;
+   using v_t = variant<T...>; // workaround a bug in icc. Can not simply replace in next line !!
+   constexpr static std::array<RType (v_t::*)(F), sizeof...(T)> table = {&v_t::template apply<T, F>...};
+   return (v.*table[v.type_id])(std::forward<F>(f));
+  }
+
+  template <typename F, typename ... T> auto visit(F &&f, variant<T...> const &v) {
+   using RType = std14::result_of_t<F(typename _first_type<T...>::type const &)>;
+   using v_t = variant<T...>;
+   constexpr static std::array<RType (v_t::*)(F) const, sizeof...(T)> table = {&v_t::template apply_c<T, F>...};
+   return (v.*table[v.type_id])(std::forward<F>(f));
+  }
+#endif
 }
 }

test/triqs/gfs/block.cpp
@@ -73,7 +73,7 @@ TEST(Gf, Block) {
   // compile only, add more test here
   auto gs1 = gf<imfreq, scalar_valued>{{beta, Fermion}};
   //auto gs1 = gf<imfreq, scalar_valued>({beta, Fermion});
-  auto bgs = make_block_gf<imfreq>(3, gs1);
+  auto bgs = make_block_gf(3, gs1);
   auto bg = reinterpret_scalar_valued_gf_as_matrix_valued(bgs);
 
   // inversion

test/triqs/utility/view_tools.cpp
@@ -18,10 +18,10 @@
  * TRIQS. If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#include <iostream>
-#include <type_traits>
 #include <triqs/arrays.hpp>
 #include <triqs/utility/view_tools.hpp>
+#include <iostream>
+#include <type_traits>
 using namespace triqs;
 using namespace triqs::arrays;
 using namespace triqs::utility;

triqs/arrays/vector.hpp
@@ -101,8 +101,13 @@ namespace arrays {
   TRIQS_DEFINE_COMPOUND_OPERATORS(vector_view);
 
   // to make interface similar to std::vector : forward [] to ()
-  template <typename Arg> auto operator[](Arg&& arg) const DECL_AND_RETURN((*this)(std::forward<Arg>(arg)));
-  template <typename Arg> auto operator[](Arg&& arg)DECL_AND_RETURN((*this)(std::forward<Arg>(arg)));
+#ifdef TRIQS_CPP11
+  template <typename Arg> auto operator[](Arg&& arg) const DECL_AND_RETURN(IMPL_TYPE::operator()(std::forward<Arg>(arg)));
+  template <typename Arg> auto operator[](Arg&& arg)DECL_AND_RETURN(IMPL_TYPE::operator()(std::forward<Arg>(arg)));
+#else
+  template <typename Arg> decltype(auto) operator[](Arg&& arg) const { return IMPL_TYPE::operator()(std::forward<Arg>(arg));}
+  template <typename Arg> decltype(auto) operator[](Arg&& arg) { return IMPL_TYPE::operator()(std::forward<Arg>(arg));}
+#endif
  };
 #undef IMPL_TYPE
 
@@ -221,8 +226,13 @@ namespace arrays {
   TRIQS_DEFINE_COMPOUND_OPERATORS(vector);
 
   // to make interface similar to std::vector : forward [] to ()
-  template <typename Arg> auto operator[](Arg&& arg) const DECL_AND_RETURN((*this)(std::forward<Arg>(arg)));
-  template <typename Arg> auto operator[](Arg&& arg)DECL_AND_RETURN((*this)(std::forward<Arg>(arg)));
+#ifdef TRIQS_CPP11
+  template <typename Arg> auto operator[](Arg&& arg) const DECL_AND_RETURN(IMPL_TYPE::operator()(std::forward<Arg>(arg)));
+  template <typename Arg> auto operator[](Arg&& arg)DECL_AND_RETURN(IMPL_TYPE::operator()(std::forward<Arg>(arg)));
+#else  
+  template <typename Arg> decltype(auto) operator[](Arg&& arg) const { return IMPL_TYPE::operator()(std::forward<Arg>(arg));}
+  template <typename Arg> decltype(auto) operator[](Arg&& arg) { return IMPL_TYPE::operator()(std::forward<Arg>(arg));}
+#endif  
 
  }; // vector class
 }

triqs/clef/sum.hpp
@@ -36,7 +36,8 @@ namespace clef {
  // requires(!triqs::clef::is_any_lazy<F, D>::value)
  auto sum_f_domain_impl(F const& f, D const& d)
      -> std::c14::enable_if_t<!triqs::clef::is_any_lazy<F, D>::value, std14::decay_t<decltype(make_regular(f(*(d.begin()))))>> {
-  auto it = d.begin(), ite = d.end();
+  auto it = d.begin(); 
+  auto ite = d.end();
   if (it == ite) TRIQS_RUNTIME_ERROR << "Sum over an empty domain";
   auto res = make_regular(f(*it));
   ++it;

triqs/gfs/curry.hpp
@@ -104,7 +104,7 @@ namespace triqs { namespace gfs {
    auto meshes_tuple = triqs::tuple::filter<pos...>(g.mesh().components());
    using var_t = cart_prod<triqs::tuple::filter_t<typename G::mesh_t::ms_tuple_t, pos...>>;
    auto m = triqs::tuple::apply_construct<gf_mesh<var_t>>(meshes_tuple);
-   auto l = [g](auto&&... x) { return partial_eval_linear_index<pos...>(g, std::make_tuple(x...)); };
+   auto l = [g](auto&&... x) { using triqs::gfs::partial_eval_linear_index; return partial_eval_linear_index<pos...>(g, std::make_tuple(x...)); };
    return make_gf_view_lambda_valued<var_t>(m, l);
   };
 

triqs/gfs/singularity/fit_tail.hpp
@@ -24,10 +24,6 @@
 
 namespace triqs { namespace gfs { 
 
- using triqs::gfs::imfreq;
- using triqs::gfs::block_index;
- using triqs::gfs::block_index;
-
  // routine for fitting the tail (singularity) of a Matsubara Green's function
  // this is a *linear* least squares problem (with non-linear basis functions)
  // which is solved by singular value decomposition of the design matrix

triqs/h5/vector.cpp
@@ -161,7 +161,7 @@ namespace h5 {
 
   dataspace d_space = H5Aget_space(attr);
 
-  mini_vector<hsize_t, 1> dims_out;
+  mini_vector<hsize_t, 2> dims_out;
   int ndims = H5Sget_simple_extent_dims(d_space, dims_out.ptr(), NULL);
   if (ndims != 2)
    TRIQS_RUNTIME_ERROR

commit 15d3cb67487ef3a924801336613912af45a24c4c

[variant] Correct dd09f78 for clang
- dd09f78 introduced an error for clang.
- 2 differents implementations of visit now :
   - gcc >= 5 && clang
   - gcc 4.9
   - icc

triqs/utility/variant.hpp
@@ -28,12 +28,6 @@
 #include <ostream>
 #include <algorithm>
 
-// a special workaround for gcc < 5.x !
-#if !defined __INTEL_COMPILER  && defined __GNUC__ && GCC_VERSION < 50100
- #define WORKAROUND_GCC49
- #warning "Compiling with workaround for gcc < 5.x"
-#endif
-
 // Forward-declarations for Boost.Serialization
 // They are neccessary along with ugly friend declarations in class variant,
 // if we want to keep the serialization code in a separate file
@@ -104,7 +98,7 @@ namespace utility {
    template <typename U> struct apply : std::is_convertible<T, U> {};
   };
 
-#if GCC_VERSION > 50200
+#if GCC_VERSION > 50100
   // Find the id of the first type T for which Predicate::apply<T>::value is
   // true, n_bounded_types if not found
   template <typename Predicate> struct find_first_id {
@@ -237,7 +231,20 @@ namespace utility {
 // Visitation
 // Return type of f(v) must be the same for any stored type, so we can use the
 // first type.
-#ifdef WORKAROUND_GCC49
+#if not defined(__INTEL_COMPILER) and GCC_VERSION >= 50100
+  template <typename F> friend auto visit(F &&f, variant &v) {
+   using RType = std14::result_of_t<F(bounded_type<0> &)>;
+   constexpr static std::array<RType (variant::*)(F), n_bounded_types> table = {&variant::apply<Types, F>...};
+   return (v.*table[v.type_id])(std::forward<F>(f));
+  }
+
+  template <typename F> friend auto visit(F &&f, variant const &v) {
+   using RType = std14::result_of_t<F(bounded_type<0> const &)>;
+  constexpr static std::array<RType (variant::*)(F) const, n_bounded_types> table = {&variant::apply_c<Types, F>...};
+   return (v.*table[v.type_id])(std::forward<F>(f));
+  }
+#endif
+#if not defined(__INTEL_COMPILER) and GCC_VERSION < 50100
   // Implementation for gcc 4.9 only.
   // A bug in gcc 4.9 (59766) forbids to use the friend auto which is clearer
   // than this. For icc, I can not leave it here, need to put it outside the class, which is ok for gcc 5.x and clang !
@@ -281,13 +288,13 @@ namespace utility {
    visit([&os](auto const &x) { os << x; }, v);
    return os;
   }
-#ifndef WORKAROUND_GCC49
+#ifdef __INTEL_COMPILER
   template <typename F, typename ... T> friend auto visit(F &&f, variant<T...> &v);
   template <typename F, typename ... T> friend auto visit(F &&f, variant<T...> const &v);
 #endif
  };
 
-#ifndef WORKAROUND_GCC49
+#ifdef __INTEL_COMPILER
 // Visitation
 // Return type of f(v) must be the same for any stored type, so we can use the
 // first type