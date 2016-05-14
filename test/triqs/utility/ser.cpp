7d2796b38b68e0e0cd17f979394750213f8eea99
Olivier Parcollet <olivier.parcollet@cea.fr>
2016-05-14 14:44:55 +0200

    Fix #302
    - If hdf5 version is too low, skip the ser.cpp test ...

--- test/triqs/utility/ser.cpp
+++ test/triqs/utility/ser.cpp
@@ -4,11 +4,15 @@
 
 TEST(Serialization, Boost) {
 
+#ifndef TRIQS_SERIALIZATION_WITH_HDF5_IMPOSSIBLE
+
  double x = 127;
  EXPECT_EQ( x, triqs::deserialize<double>(triqs::serialize(x)));
 
  auto v = std::vector<std::string> {"abc","3"};
  EXPECT_EQ( v, triqs::deserialize<std::vector<std::string>>(triqs::serialize(v)));
+
+#endif
 
 }
 MAKE_MAIN;
