1717a9194f9f3b5c5b8c1e7af418225fa428c084
tayral <thomas.ayral@polytechnique.edu>
2016-05-23 17:37:57 +0200

    [tests] add app_name to moduleB test.

--- test/pytriqs/wrap_test/my_moduleB_desc.py
+++ test/pytriqs/wrap_test/my_moduleB_desc.py
@@ -1,7 +1,7 @@
 from wrap_generator import *
 
 # The module
-mod = module_(full_name = "my_moduleB", doc = " Doc of my_module ")
+mod = module_(full_name = "my_moduleB", doc = " Doc of my_module ", app_name="triqs")
 mod.use_module('my_module')
 mod.add_include("<triqs/../test/pytriqs/wrap_test/b.hpp>") 
 mod.add_include("<triqs/py_converters/my_module.hpp>")
