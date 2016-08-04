bab2b51b4ed81489c52030f0770c14e579b10705
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-08-04 15:25:30 +0200

    [arrays] Fix #351
    * Clean up stev.hpp
    * Port 'tridiag' test to GTest.

--- test/triqs/arrays/tridiag.cpp
+++ test/triqs/arrays/tridiag.cpp
@@ -18,67 +18,58 @@
  * TRIQS. If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#include "./common.hpp"
+#include <triqs/test_tools/arrays.hpp>
 #include <triqs/arrays/blas_lapack/stev.hpp>
 #include <triqs/utility/is_complex.hpp>
+#include <triqs/utility/numeric_ops.hpp>
 
 using namespace triqs::arrays;
 using namespace triqs;
 using dcomplex = std::complex<double>;
 
-double _conj(double x) { return x; }
-dcomplex _conj(dcomplex x) { return conj(x); }
-
 template <typename T> void check_eig(matrix<T> M, matrix_view<T> vectors, vector_view<double, 1> values) {
  auto _ = range();
  for (auto i : range(0,first_dim(M))) {
-  std::cerr << "check "<< i << std::endl;
-  std::cerr << (M -values(i))* vectors(i, _)<<std::endl;
-  assert_all_close(M * vectors(i, _), values(i) * vectors(i, _), 1.e-14);
+  EXPECT_ARRAY_NEAR(M * vectors(i, _), values(i) * vectors(i, _), 1.e-14);
  }
 }
 
 template<typename Md, typename Me> void test(Md d, Me e) {
  using value_t = typename Me::value_type;
+ using triqs::utility::conj;
 
  matrix<value_t> A(first_dim(d),first_dim(d));
  assign_foreach(A,[&](size_t i, size_t j){
      return j==i ? d(i) :
             j==i+1 ? e(i) :
-            j==i-1 ? _conj(e(j)) :
+            j==i-1 ? conj(e(j)) :
             .0;
  });
 
- blas::tridiag_worker<triqs::is_complex<value_t>::value> w(2*first_dim(d));
- w(d,e);
-
- std::cerr << "A = " << A << std::endl;
- std::cerr << " values = " << w.values() << std::endl;
- std::cerr << " vectors = " << w.vectors() << std::endl;
- check_eig(A, w.vectors(), w.values());
+ int size = first_dim(d);
+ for(int init_size : {0, size, 2*size}) {
+  blas::tridiag_worker<triqs::is_complex<value_t>::value> w(init_size);
+  w(d,e);
+  check_eig(A, w.vectors(), w.values());
+ }
 }
 
-int main(int argc, char **argv) {
-
- // Real symmetric matrix
- {
-  vector<double> d(5);
-  vector<double> e(4);
-  assign_foreach(d,[](size_t i){ return 3.2*i+2.3; });
-  assign_foreach(e,[](size_t i){ return 2.4*(i+1.82) + 0.78; });
-  e(1) = .0;
-  test(d,e);
- }
-
- // Complex Hermitian matrix
- {
-  vector<double> d(5);
-  vector<dcomplex> e(4);
-  assign_foreach(d,[](size_t i){ return 3.2*i+2.3; });
-  assign_foreach(e,[](size_t i){ return 2.4*(i+1.82) + 0.78*dcomplex(0,1.0); });
-  e(1) = .0;
-  test(d,e);
- }
-
- return 0;
+TEST(tridiag, real) {
+ vector<double> d(5);
+ vector<double> e(4);
+ assign_foreach(d,[](size_t i){ return 3.2*i+2.3; });
+ assign_foreach(e,[](size_t i){ return 2.4*(i+1.82) + 0.78; });
+ e(1) = .0;
+ test(d,e);
 }
+
+TEST(tridiag, complex) {
+ vector<double> d(5);
+ vector<dcomplex> e(4);
+ assign_foreach(d,[](size_t i){ return 3.2*i+2.3; });
+ assign_foreach(e,[](size_t i){ return 2.4*(i+1.82) + 0.78*dcomplex(0,1.0); });
+ e(1) = .0;
+ test(d,e);
+}
+
+MAKE_MAIN;
--- triqs/arrays/blas_lapack/stev.hpp
+++ triqs/arrays/blas_lapack/stev.hpp
@@ -18,20 +18,20 @@
  * TRIQS. If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#ifndef TRIQS_ARRAYS_BLAS_LAPACK_DSTEV_H
-#define TRIQS_ARRAYS_BLAS_LAPACK_DSTEV_H
+#pragma once
+
 #include "./tools.hpp"
 
-namespace triqs { namespace arrays { namespace blas { 
+namespace triqs { namespace arrays { namespace blas {
 
  using namespace blas_lapack_tools;
  namespace f77 { // overload
 
-  extern "C" { 
+  extern "C" {
    void TRIQS_FORTRAN_MANGLING(dstev)(const char *, const int &N, const double [], const double [], const double [],  const int &, double [], int &);
   }
 
-  inline void stev (char J, const int & N, double* D, double* E, double* Z,  int ldz, double * work, int & info)  { 
+  inline void stev (char J, const int & N, double* D, double* E, double* Z,  int ldz, double * work, int & info)  {
    TRIQS_FORTRAN_MANGLING(dstev)(&J,N,D,E,Z,ldz,work,info);
   }
  }
@@ -39,60 +39,60 @@ namespace triqs { namespace arrays { namespace blas {
  template<bool Complex = false> struct tridiag_worker {};
 
  template<> struct tridiag_worker<false> {
-  arrays::vector<double> D,E,W;
+  vector<double> D,E,W;
   matrix<double> Z;
   size_t s;
-  tridiag_worker (size_t n){ s=0; resize(n);}
-  void resize(size_t n) { if (s<n) {D.resize(n); E.resize(n); W.resize(2*n+1); Z.resize(n,n);} s = n; }
-  template<typename VT> 
-   void operator()(VT const & d, VT const & e) { 
-    if (e.size() != d.size() -1) TRIQS_RUNTIME_ERROR << " Error in tridiagonal matrix diagonalization size mismatch"; 
-    resize(d.size());
-    D(range(0,s)) = d; 
-    E(range(0,s-1)) = e;
-    int info;
-    f77::stev ('V',d.size(),D.data_start(),E.data_start(), Z.data_start(), Z.shape(0),W.data_start(), info);
-    if (info !=0) TRIQS_RUNTIME_ERROR << " Error in tridiagonal matrix diagonalization "<< info;
-   }
-  arrays::vector_view<double> values() const { return D(range(0,s));}
+  tridiag_worker(size_t n) : s(0) { resize(n); }
+  void resize(size_t n) {
+   if(D.size()<n) { D.resize(n); E.resize(n); W.resize(std::max(1,2*int(n)-2)); Z.resize(n,n); }
+   s = n;
+  }
+  template<typename VT> void operator()(VT const & diag, VT const & supdiag /* superdiagonal */) {
+   if (supdiag.size() != diag.size() - 1) TRIQS_RUNTIME_ERROR << "Error in tridiagonal matrix diagonalization: size mismatch";
+   resize(diag.size());
+   D(range(0,s)) = diag;
+   E(range(0,s-1)) = supdiag;
+   int info;
+   f77::stev('V', s, D.data_start(), E.data_start(), Z.data_start(), first_dim(Z), W.data_start(), info);
+   if(info != 0) TRIQS_RUNTIME_ERROR << "Error in tridiagonal matrix diagonalization " << info;
+  }
+  vector_view<double> values() const { return D(range(0,s));}
   matrix_view<double> vectors() const { return Z(range(0,s),range(0,s));}
  };
 
  template<> struct tridiag_worker<true> {
-  arrays::vector<double> D,E,W;
+  vector<double> D,E,W;
   matrix<double> Z;
-  arrays::vector<std::complex<double>> U; // similarity transformation
-  matrix<std::complex<double>> V;         // transformed eigenvectors
+  vector<std::complex<double>> U; // similarity transformation
+  matrix<std::complex<double>> V; // transformed eigenvectors
   size_t s;
-  tridiag_worker (size_t n){ s=0; resize(n);}
-  void resize(size_t n) { if (s<n) {D.resize(n); E.resize(n); W.resize(2*n+1); Z.resize(n,n); U.resize(n); V.resize(n,n);} s = n; }
-  template<typename VTd, typename VTe>
-   void operator()(VTd const & diag, VTe const & supdiag /* superdiagonal */) {
-    if (supdiag.size() != diag.size() -1) TRIQS_RUNTIME_ERROR << " Error in tridiagonal matrix diagonalization size mismatch";
-    resize(diag.size());
-    D(range(0,s)) = diag;
-    // construct transformed off-diagonal elements;
-    auto U_it = std::begin(U);
-    auto E_it = std::begin(E);
-    *U_it = 1.0;
-    for(auto e : supdiag){
-      auto U_old = *U_it;
-      ++U_it;
-      *U_it = U_old*(std::isnormal(abs(e)) ? conj(e)/abs(e) : 1.0);
-      *E_it = std::real(e*conj(U_old)*(*U_it));
-      ++E_it;
-    }
-    int info;
-    f77::stev ('V',diag.size(),D.data_start(),E.data_start(), Z.data_start(), Z.shape(0),W.data_start(), info);
-    if (info !=0) TRIQS_RUNTIME_ERROR << " Error in tridiagonal matrix diagonalization "<< info;
-
-    // Back-transform the eigenvectors
-    assign_foreach(V, [this](size_t i, size_t j) { return Z(i,j)*conj(U(i))*U(j); });
+  tridiag_worker(size_t n) : s(0) { resize(n);}
+  void resize(size_t n) {
+   if (D.size()<n) {D.resize(n); E.resize(n); W.resize(std::max(1,2*int(n)-2)); Z.resize(n,n); U.resize(n); V.resize(n,n);}
+   s = n;
+  }
+  template<typename VTd, typename VTe> void operator()(VTd const & diag, VTe const & supdiag /* superdiagonal */) {
+   if (supdiag.size() != diag.size() - 1) TRIQS_RUNTIME_ERROR << "Error in tridiagonal matrix diagonalization: size mismatch";
+   resize(diag.size());
+   D(range(0,s)) = diag;
+   // construct transformed off-diagonal elements;
+   U(0) = 1.0;
+   for(int i : range(0,s-1)) {
+    std::complex<double> e = supdiag(i);
+    U(i+1) = U(i)*(std::isnormal(abs(e)) ? conj(e)/abs(e) : 1.0);
+    E(i) = std::real(e*conj(U(i))*(U(i+1)));
    }
-  arrays::vector_view<double> values() const { return D(range(0,s));}
-  matrix_view<std::complex<double>> vectors() const { return V(range(0,s),range(0,s));}
+   int info;
+   f77::stev('V', s, D.data_start(), E.data_start(), Z.data_start(), first_dim(Z), W.data_start(), info);
+   if (info != 0) TRIQS_RUNTIME_ERROR << "Error in tridiagonal matrix diagonalization " << info;
+
+   // Back-transform the eigenvectors
+   for(int i : range(0,s))
+    for(int j : range(0,s))
+     V(i,j) = Z(i,j)*conj(U(i))*U(j);
+  }
+  vector_view<double> values() const { return D(range(0,s)); }
+  matrix_view<std::complex<double>> vectors() const { return V(range(0,s),range(0,s)); }
  };
 
 }}}// namespace
-#endif
-
