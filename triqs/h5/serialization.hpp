ab3bd377a931b3940d8a7563720d0e013accf58c
Olivier Parcollet <olivier.parcollet@cea.fr>
2017-07-03 22:19:29 +0200

    [arrays] remove unused files

--- triqs/h5/serialization.hpp
+++ triqs/h5/serialization.hpp
@@ -2,7 +2,7 @@
  *
  * TRIQS: a Toolbox for Research in Interacting Quantum Systems
  *
- * Copyright (C) 2012 by M. Ferrero, O. Parcollet
+ * Copyright (C) 2011-2017 by O. Parcollet
  *
  * TRIQS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
