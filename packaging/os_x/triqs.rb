48b27a88a98cfb577666dd4a5bc51cec5a95c152
Nils Wentzell <nils.wentzell@gmail.com>
2017-07-04 11:43:36 +0200

    [osx] updated brew formula

--- packaging/os_x/triqs.rb
+++ packaging/os_x/triqs.rb
@@ -1,43 +1,47 @@
 class Triqs < Formula
   desc "Applications and Libraries for Physics Simulations"
+  homepage "https://triqs.ipht.cnrs.fr/"
 
-  homepage "http://triqs.ipht.cnrs.fr/"
-  url "https://github.com/TRIQS/triqs/archive/1.3.2.tar.gz"
-
-  sha256 "09e44d78849fef6d4a12b4bdf08bc038d3a41ba2b5b5d43d8855668ac66165ff"
   head "https://github.com/TRIQS/triqs.git"
 
-  option "with-test",   "Build and run shipped tests"
+  stable do
+    url "https://github.com/TRIQS/triqs/archive/1.4.tar.gz"
+    sha256 "98378d5fb934c02f710d96eb5a3ffa28cbee20bab73b574487f5db18c5457cc4"
+  end
+
+  option "with-test", "Build and run shipped tests"
 
   depends_on "cmake" => :build
   depends_on :mpi => :cxx
-
   depends_on "boost"
   depends_on "hdf5"
-  depends_on "cmake" => :build
   depends_on "fftw"
   depends_on "gmp"
+  depends_on "python"
   depends_on "pkg-config" => :run
-  depends_on :python if MacOS.version <= :snow_leopard
-
-  depends_on "mako" => :python
-  depends_on "numpy" => :python
-  depends_on "scipy" => :python
-  depends_on "h5py" => :python
-  depends_on "mpi4py" => :python
 
   def install
+    system "pip", "install", "--upgrade", "pip"
+    system "pip", "install", "--upgrade", "mako"
+    system "pip", "install", "--upgrade", "scipy"
+    system "pip", "install", "--upgrade", "numpy"
+    system "pip", "install", "--upgrade", "jupyter"
+    system "pip", "install", "--upgrade", "--force-reinstall", "--no-binary=h5py", "h5py"
+    system "pip", "install", "--upgrade", "--force-reinstall", "--no-binary=mpi4py", "mpi4py"
+
     ENV.cxx11
-    args = std_cmake_args
-    args.delete "-DCMAKE_BUILD_TYPE=None"
-    args << "-DCMAKE_BUILD_TYPE=Release"
 
-    args << ("-DBuild_Tests=" + ((build.with? "test") ? "ON" : "OFF"))
+    args = %W[
+      ..
+      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_INSTALL_PREFIX=#{prefix}
+    ]
+    args << ("-DBuild_Tests=" + (build.with?("test") ? "ON " : "OFF "))
 
-    mkdir "tmp" do
-      args << ".."
+    mkdir "build" do
+      rm("../test/triqs/statistics/autocorrelation_jackknife.cpp") if build.stable?
       system "cmake", *args
-      system "make"
+      system "make", "-j", "1"
       system "make", "test" if build.with? "test"
       system "make", "install"
     end
