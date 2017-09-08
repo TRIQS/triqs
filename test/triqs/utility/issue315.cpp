c7d1b6a85213f843d04dee9b1082c786398206f3
Michel Ferrero <mferrero@cpht.polytechnique.fr>
2017-09-08 17:42:05 +0200

    Fix #315

---
+++ test/triqs/utility/issue315.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <triqs/utility/time_pt.hpp>
+
+using triqs::utility::time_pt;
+using triqs::utility::time_segment;
+
+int main(int argc, char **argv)
+{
+
+ time_segment seg(10);
+
+ time_pt zero_pt = seg.get_lower_pt();
+ time_pt beta_pt = seg.get_upper_pt();
+ time_pt pt = seg.make_time_pt(3);
+
+ std::cout << beta_pt << std::endl;
+
+ std::cout << pt + zero_pt << std::endl;
+ std::cout << pt + beta_pt << std::endl;
+ std::cout << pt - zero_pt << std::endl;
+ std::cout << pt - beta_pt << std::endl;
+
+ std::cout << zero_pt + zero_pt << std::endl;
+ std::cout << zero_pt + beta_pt << std::endl;
+ std::cout << beta_pt + zero_pt << std::endl;
+ std::cout << beta_pt + beta_pt << std::endl;
+
+ std::cout << zero_pt - zero_pt << std::endl;
+ std::cout << zero_pt - beta_pt << std::endl;
+ std::cout << beta_pt - zero_pt << std::endl;
+ std::cout << beta_pt - beta_pt << std::endl;
+
+ return 0;
+
+}
