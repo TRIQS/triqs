dbf1756e1ebe1561f7aa0b0f2e79ba9e2893a372
Olivier Parcollet <olivier.parcollet@cea.fr>
2017-11-07 11:41:25 +0100

    [gf] remove < operator for matsubara_freq.
    - what does it exactly mean ?
    - seems not used.

--- triqs/gfs/domains/matsubara.hpp
+++ triqs/gfs/domains/matsubara.hpp
@@ -76,13 +76,13 @@ namespace gfs {
    return std::complex<double>{x} * std::complex<double>{y}; 
  }
 
- template<typename T> bool operator<(matsubara_freq const &x, T const &y) { return (x.n <y);}
- template<typename T> bool operator<(T const &x, matsubara_freq const &y) { return (x <y.n);}
- inline bool operator<(matsubara_freq const &x, matsubara_freq const &y) { return (x.n<y.n);}
+ //template<typename T> bool operator<(matsubara_freq const &x, T const &y) { return (x.n <y);}
+ //template<typename T> bool operator<(T const &x, matsubara_freq const &y) { return (x <y.n);}
+ //inline bool operator<(matsubara_freq const &x, matsubara_freq const &y) { return (x.n<y.n);}
  
- template<typename T> bool operator>(matsubara_freq const &x, T const &y) { return (x.n>y);}
- template<typename T> bool operator>(T const &x, matsubara_freq const &y) { return (x>y.n);}
- inline bool operator>(matsubara_freq const &x, matsubara_freq const &y) { return (x.n>y.n);}
+ //template<typename T> bool operator>(matsubara_freq const &x, T const &y) { return (x.n>y);}
+ //template<typename T> bool operator>(T const &x, matsubara_freq const &y) { return (x>y.n);}
+ //inline bool operator>(matsubara_freq const &x, matsubara_freq const &y) { return (x.n>y.n);}
 
  //---------------------------------------------------------------------------------------------------------
  /// The domain
