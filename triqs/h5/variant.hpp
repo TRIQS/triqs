9ac4fc289687fe29a9206e6f7d2536c5cc25c315
Olivier Parcollet <olivier.parcollet@cea.fr>
2017-12-07 18:44:31 +0100

    WIP

---
+++ triqs/h5/variant.hpp
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ *
+ * TRIQS: a Toolbox for Research in Interacting Quantum Systems
+ *
+ * Copyright (C) 2017 by O. Parcollet
+ *
+ * TRIQS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TRIQS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+#include "./group.hpp"
+#include "./string.hpp"
+#include "./base.hpp"
+#include <variant>
+
+namespace std { // to be found by ADL
+
+  template <typename... T> std::string get_triqs_hdf5_data_scheme(std::variant<T...> const &v) {
+    auto l = [](auto &&x) {
+      using triqs::get_triqs_hdf5_data_scheme; // for the basic types, not found by ADL
+      return get_triqs_hdf5_data_scheme(x);
+    };
+    return visit(l, v);
+  }
+} // namespace std
+
+namespace triqs::h5 {
+
+  /**
+   * Optional : write if the value is set.
+   */
+  template <typename... T> void h5_write(h5::group gr, std::string const &name, std::variant<T...> const &v) {
+    visit([&](auto const &x) { h5_write(gr, name, x); }, v);
+  }
+
+  /**
+   * Read optional from the h5
+   */
+  template <typename... T> void h5_read(h5::group gr, std::string name, std::variant<T...> &v) {
+    visit([&](auto &x) { 
+	
+        dataset ds = gr.open_dataset(name);
+ 	std::cout  << " eq " << H5Tequal(H5Dget_type(ds), h5_type_from_C(x))<<std::endl;;
+
+	
+	h5_read(gr, name, x); }, v);
+  }
+} // namespace triqs::h5
