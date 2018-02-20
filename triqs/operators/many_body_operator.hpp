TRIQS/triqs

commit 4c5b76344dab6a15b94ea55dd643d65fe02c1ef6

[h5] Bugfixes, changing registration
- replace get_triqs_hdf5_scheme ->
  1 function (get_hdf5_scheme<T>)
  1 struct hdf5_scheme_impl<T> to specialize (cf.
triqs/h5/base_public.hpp)
  1 static std::string hdf5_scheme() {....} in each class we control

- HDFArchive : autorize partial loading of objects:

  If an object S (e.g. solver ?) contains subgroups or object
  e.g. g, or a
  then arch['S/a'], arch['S/g'] retrieves only this part.

  Also forbid / in keys in hdf5 files, which would be very confusing.
  Should not break backward compatibility.

- Save/Read bool as a H5T_ENUM for boolean compatible with h5py + Test

- Make std::tuple write/readable + Test

- Update hdf5 python and cpp tests

- We now have the following correspondences

  HDF5			| Python  |     C++
  -------------------------------------------------
  PythonListWrap 	  List 		std::vector< .. >
  PythonTupleWrap 	  Tuple 	std::tuple< .. >
  PythonDictWrap 	  Dict 	       	std::map< .. >

- Fixed a bug where, when reading a map in h5, the subgroups were not covered.

- Minor bugfixes

Co-authored-by: Olivier Parcollet <olivier.parcollet@cea.fr>

triqs/operators/many_body_operator.hpp
@@ -44,7 +44,6 @@ namespace operators {
  using many_body_operator = many_body_operator_generic<real_or_complex>;
  using many_body_operator_real = many_body_operator_generic<double>;
  using many_body_operator_complex = many_body_operator_generic<std::complex<double>>;
- inline std::string get_triqs_hdf5_data_scheme(many_body_operator const&) { return "Operator"; }
 
  //-----------------------------------------------------------------------------------------
 
@@ -109,6 +108,8 @@ namespace operators {
   public:
   using scalar_t = ScalarType;
 
+  static std::string hdf5_scheme() {return  "Operators";}
+  
   many_body_operator_generic() = default;
   many_body_operator_generic(many_body_operator_generic const&) = default;
   many_body_operator_generic(many_body_operator_generic&&) = default;

pytriqs/archive/hdf_archive.py
@@ -155,6 +155,7 @@ class HDFArchiveGroup (HDFArchiveGroupBasicLayer) :
 
     #-------------------------------------------------------------------------
     def __setitem__(self,key,val) :
+        assert '/' not in key, "/ can not be part of a key"
         key= self._key_cipher(key)# first look if key is a string or key
 
         if key in self.keys() :
@@ -219,6 +220,11 @@ class HDFArchiveGroup (HDFArchiveGroupBasicLayer) :
     def __getitem__(self,key) :
         """Return the object key, possibly reconstructed as a python object if
         it has been properly set up"""
+        # If the key contains /, grabs the subgroups
+        if '/' in key:
+            a,l =self, key.split('/')
+            for s in l[:-1]: a = a.get_raw(s)
+            return a[l[-1]]
         return self.__getitem1__(key,self._reconstruct_python_objects)
 
     #-------------------------------------------------------------------------

pytriqs/archive/hdf_archive_basic_layer_h5py.py
@@ -85,6 +85,7 @@ class HDFArchiveGroupBasicLayer :
             if type(v) in [n.uint8, n.uint16, n.uint32, n.uint64] : return int(v)
             if type(v) in [n.float,  n.float32, n.float64] : return float(v)
             if type(v) in [n.complex, n.complex64, n.complex128] : return complex(v)
+            if type(v) in [n.bool_] : return bool(v)
             if type(v) in [n.str_, n.string_] : return str(v)
             # converts the array of numpy string into a list of normal python strings
             if type(v) is n.ndarray and v.dtype.type is n.string_ : return map(str,v)

pytriqs/archive/hdf_archive_schemes.py
@@ -29,11 +29,11 @@ class HDF5Scheme :
         return """
         Name of the class : %s
         Name of the module : %s
-        Documentation : %s"""(self.classname,self.modulename,self.doc)
+        Documentation : %s"""%(self.classname,self.modulename,self.doc)
         
 _hdf5_schemes_dict= {}
 
-def register_class (cls, doc = None, read_fun = None):
+def register_class (cls, doc = None, read_fun = None, hdf5_data_scheme = None):
     """
      For each class, register it with::
 
@@ -41,7 +41,7 @@ def register_class (cls, doc = None, read_fun = None):
          register_class (GfImFreq, doc= doc_if_different_from cls._hdf5_data_scheme_doc_ )
 
     """
-    hdf_scheme = cls._hdf5_data_scheme_ if hasattr(cls,"_hdf5_data_scheme_") else cls.__name__ 
+    hdf_scheme = hdf5_data_scheme or (cls._hdf5_data_scheme_ if hasattr(cls,"_hdf5_data_scheme_") else cls.__name__) 
     assert hdf_scheme not in _hdf5_schemes_dict, "class %s is already registered"%hdf_scheme
     doc = doc if doc else (cls._hdf5_data_scheme_doc_ if hasattr(cls,"_hdf5_data_scheme_doc_") else {})
     _hdf5_schemes_dict [hdf_scheme] = HDF5Scheme (cls.__name__, cls.__module__,doc, hdf_scheme, read_fun)

test/pytriqs/base/CMakeLists.txt
@@ -9,7 +9,8 @@ add_python_test(test_example)
 add_python_test(operator_python_test)
 add_python_test(operator_python2)
 
-# Some basic HDF IO stuff
+# Some basic HDF5 Stuff
+add_python_test(hdf5_bool)
 add_python_test(hdf5_io)
 
 # Greens functions

test/pytriqs/base/hdf5_bool.py
@@ -1,0 +1,49 @@
+################################################################################
+#
+# TRIQS: a Toolbox for Research in Interacting Quantum Systems
+#
+# Copyright (C) 2017 by N. Wentzell, O. Parcollet
+#
+# TRIQS is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# TRIQS. If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+import unittest
+import numpy as np
+
+from pytriqs.archive import *
+
+class TestHdf5Bool(unittest.TestCase):
+
+    def test_write_read_bool(self):
+
+        # Write
+        with HDFArchive('bool.h5','w') as arch: 
+            arch['t'] = True
+            arch['f'] = False 
+            arch['i'] = 10
+
+        with HDFArchive('bool.h5','r') as arch: 
+            t = arch['t']
+            f = arch['f'] 
+            i = arch['i']
+
+        self.assertTrue(t) 
+        self.assertFalse(f)
+        self.assertIs(type(t),bool)
+        self.assertIs(type(f),bool)
+        self.assertIs(type(i),int)
+
+if __name__ == '__main__':
+    unittest.main()

test/pytriqs/base/hdf5_io.py
@@ -20,53 +20,67 @@
 #
 ################################################################################
 
-from pytriqs.archive import *
+import unittest
 from numpy import *
+
+from pytriqs.archive import *
 from pytriqs.utility.comparison_tests import *
 
-d = {'a' : 1.0, 'b' : [1,2,3]}
+class TestHdf5Io(unittest.TestCase):
 
-with HDFArchive('hdf5_io.out.h5','w', init = d.items()) as h:
-    h['c'] = 100
-    h['d'] = array([[1,2,3],[4,5,6]])
-    h['e'] = (1,2,3)
-    h['f'] = { 'a':10, 'b': 20}
-    h.create_group('g')
-    g = h['g']
-    g['a'] = 98
-    g['b'] = (1,2,3)
-    g['c'] = 200
+    def test_hdf5_io(self):
+        d = {'dbl' : 1.0, 'lst' : [1,[1],'a']}
+        
+        # === Write to archive
+        with HDFArchive('hdf5_io.out.h5','w', init = d.items()) as arch:
+        
+            arch['int'] = 100
+            arch['arr'] = array([[1,2,3],[4,5,6]])
+            arch['tpl'] = (2,[2],'b')
+            arch['dct'] = { 'a':[10], 'b':20 }
+        
+            arch.create_group('grp')
+            grp = arch['grp']
+            grp['int'] = 98
+            grp['tpl'] = (3,[3],'c')
+            grp['dct'] = { 'a':[30], 'b':40 }
+        
+        # === Read access
+        with HDFArchive('hdf5_io.out.h5','r') as arch:  
+            dct = arch['dct']
+            grp = arch['grp']
+        
+        # === Read/Write access
+        with HDFArchive('hdf5_io.out.h5','a') as arch:
+        
+            dct = arch['dct']
+            dct['c'] = 'triqs'
+            arch['dct'] = dct
+        
+            grp = arch['grp']
+            dct = grp['dct']
+            dct['c'] = 'qmc'
+        
+            grp['dct'] = dct
+            grp['d'] = 700
+            grp['x'] = 1.5
+            grp['y'] = 'zzz'
+        
+        
+        # === Final check
+        arch = HDFArchive('hdf5_io.out.h5','r')
+        self.assertEqual( arch['dbl'] , 1.0 )
+        self.assertEqual( arch['lst'] , [1,[1],'a'] )
+        self.assertEqual( arch['int'] , 100 )
+        assert_arrays_are_close( arch['arr'] , array([[1, 2, 3], [4, 5, 6]]) )
+        self.assertEqual( arch['tpl'] , (2,[2],'b') )
+        self.assertEqual( arch['dct'] , {'a':[10], 'b':20, 'c':'triqs'} )
+        self.assertEqual( arch['grp']['int'] , 98 )
+        self.assertEqual( arch['grp']['tpl'] , (3,[3],'c') )
+        self.assertEqual( arch['grp']['dct'] , { 'a':[30], 'b':40, 'c':'qmc'} )
+        self.assertEqual( arch['grp']['d'] , 700 )
+        self.assertEqual( arch['grp']['x'] , 1.5 )
+        self.assertEqual( arch['grp']['y'] , 'zzz' )
 
-h2 = HDFArchive('hdf5_io.out.h5','r')
-dd = h2['f']
-gg = h2['g']
-del h2, gg
-
-h3 = HDFArchive('hdf5_io.out.h5','a')
-dd = h3['f']
-dd['c'] = 18
-h3['f'] = dd
-gg = h3['g']
-gg['d'] = 700
-del h3, gg
-
-gg = HDFArchive('hdf5_io.out.h5','a')['g']
-gg['x'] = 1.5
-
-with HDFArchive('hdf5_io.out.h5','a')['g'] as g:
-    g['y'] = 'zzz'
-
-# Final check
-A = HDFArchive('hdf5_io.out.h5','r')
-assert A['a'] == 1.0
-assert A['b'] == [1,2,3]
-assert A['c'] == 100
-assert_arrays_are_close(A['d'], array([[1, 2, 3], [4, 5, 6]]))
-assert A['e'] == (1,2,3)
-assert A['f'] == {'a': 10, 'c': 18, 'b': 20}
-assert A['g']['a'] == 98
-assert A['g']['b'] == (1, 2, 3)
-assert A['g']['c'] == 200
-assert A['g']['d'] == 700
-assert A['g']['x'] == 1.5
-assert A['g']['y'] == 'zzz'
+if __name__ == '__main__':
+    unittest.main()

test/pytriqs/wrap_test/a.hpp
@@ -113,12 +113,12 @@ struct A {
   }
 
   ///
-  friend std::string get_triqs_hdf5_data_scheme(A const&) { return "Ac"; }
+  static std::string hdf5_scheme() { return "Ac"; }
 
   /// Write into HDF5
   friend void h5_write(triqs::h5::group fg, std::string subgroup_name, A const &a) {
    auto gr = fg.create_group(subgroup_name);
-   gr.write_triqs_hdf5_data_scheme(a);
+   gr.write_hdf5_scheme(a);
    h5_write(gr, "x", a.x);
   }
 

commit da793fbdb2cd9375cfd184631751bcb091b0b841

[h5] Fixing bug in operator label introduced in commit 4c5b7634

triqs/operators/many_body_operator.hpp
@@ -115,7 +115,7 @@ namespace operators {
   public:
   using scalar_t = ScalarType;
 
-  static std::string hdf5_scheme() {return  "Operators";}
+  static std::string hdf5_scheme() {return  "Operator";}
   
   many_body_operator_generic() = default;
   many_body_operator_generic(many_body_operator_generic const&) = default;