c507a9088581e63e3a83afcd3ccbfa82a6518419
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-05-23 15:17:19 -0400

    [test] gf_density boson test fails! n = infty...

--- test/triqs/gfs/gf_density.cpp
+++ test/triqs/gfs/gf_density.cpp
@@ -1,6 +1,6 @@
 #include <triqs/test_tools/gfs.hpp>
 
-TEST(Gf, Density) {
+TEST(Gf, DensityFermion) {
 
   double beta   = 1;
   int n_im_freq = 400;
@@ -14,6 +14,24 @@ TEST(Gf, Density) {
   auto n = triqs::gfs::density(G);
 
   EXPECT_COMPLEX_NEAR(n, 1 / (1 + std::exp(-beta * 2.3)), 1.e-9);
+
+  rw_h5(G);
+}
+
+TEST(Gf, DensityBoson) {
+
+  double beta   = 1;
+  int n_im_freq = 400;
+  auto G        = gf<imfreq, scalar_valued>{{beta, Boson, n_im_freq}};
+
+  EXPECT_EQ(G.mesh().positive_only(), 0);
+
+  triqs::clef::placeholder<0> om_;
+  G(om_) << 1 / (om_ - 2.3); // nb! need positive energy for a free boson
+
+  auto n = triqs::gfs::density(G);
+
+  EXPECT_COMPLEX_NEAR(n, 1 / (-1 + std::exp(beta * 2.3)), 1.e-9);
 
   rw_h5(G);
 }
