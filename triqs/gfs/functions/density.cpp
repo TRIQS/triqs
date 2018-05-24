1e3ee8e10d9399011b466c193d77c1c9363aeb8a
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-05-24 10:44:00 -0400

    [gf] density for bosonic Gf

--- triqs/gfs/functions/density.cpp
+++ triqs/gfs/functions/density.cpp
@@ -3,6 +3,7 @@
  * TRIQS: a Toolbox for Research in Interacting Quantum Systems
  *
  * Copyright (C) 2012 by M. Ferrero, O. Parcollet
+ * Copyright (C) 2018 The Simons Foundation, Author: H. UR Strand
  *
  * TRIQS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -45,16 +46,53 @@ namespace triqs::gfs {
     int N1 = sh[0], N2 = sh[1];
     arrays::matrix<dcomplex> res(sh);
     auto beta = g.domain().beta;
-    double b1 = 0, b2 = 1, b3 = -1;
-    auto F = [&beta](dcomplex a, double b) { return -a / (1 + exp(-beta * b)); };
+
+    auto S = g.mesh().domain().statistic;
+    double b1, b2, b3; // pole location for tail model
+    double xi; // +1, -1 for boson/fermion
+
+    if( S == Fermion ) {
+      xi = -1.;
+      b1 = 0; b2 = 1; b3 = -1;
+    } else if( S == Boson ) {
+      xi = 1.;
+      b1 = -1.; b2 = 1.; b3 = 1./2.;
+    } else
+      TRIQS_RUNTIME_ERROR << "ERROR: Unknown statistic in density\n";
+
+    // exact expression for sum over Matsubara frequencies for a single pole
+    // located at b with amplitude a
+    auto F = [&beta, &xi](dcomplex a, double b) { return xi * a / (-xi + exp(-beta * b)); };
 
     for (int n1 = 0; n1 < N1; n1++)
       for (int n2 = n1; n2 < N2; n2++) {
         dcomplex m1 = tail(1, n1, n2), m2 = tail(2, n1, n2), m3 = tail(3, n1, n2);
-        dcomplex a1 = m1 - m3, a2 = (m2 + m3) / 2, a3 = (m3 - m2) / 2;
-        dcomplex r = 0;
+
+	// inverse of the Vandermonte matrix
+	//
+	// V =
+	// [ 1,    1,    1    ]
+	// [ b1,   b2,   b3   ]
+	// [ b1^2, b2^2, b3^2 ]
+	//
+	// V * a = m => a = V^{-1} * m
+
+	dcomplex a1, a2, a3; // amplitudes for each pole in tail model
+	
+	if( S == Fermion ) {
+	  a1 = m1 - m3; a2 = (m2 + m3) / 2; a3 = (m3 - m2) / 2;
+	} else if ( S == Boson ) {
+	  a1 = m1/6. - m2/2. + m3/3.;
+	  a2 = -m1/2. + m2/2. + m3;
+	  a3 = 4.*m1/3. - 4.*m3/3.;
+	} else
+	  TRIQS_RUNTIME_ERROR << "ERROR: Unknown statistic in density\n";
+
+	dcomplex r = 0;
         for (auto const &w : g.mesh()) r += g[w](n1, n2) - (a1 / (w - b1) + a2 / (w - b2) + a3 / (w - b3));
         res(n1, n2) = r / beta + m1 + F(a1, b1) + F(a2, b2) + F(a3, b3);
+	res(n1, n2) *= -xi;
+
         if (n2 > n1) res(n2, n1) = conj(res(n1, n2));
       }
 
