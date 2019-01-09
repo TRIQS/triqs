64f1505433e61bc203b932a520c401e7bf1c1361
Alice Moutenet <alice.moutenet@gmail.com>
2019-01-09 11:36:14 -0500

    [mc] fix make measure timer optional

--- triqs/mc_tools/mc_measure_set.hpp
+++ triqs/mc_tools/mc_measure_set.hpp
@@ -43,7 +43,7 @@ namespace triqs {
       utility::timer Timer;
 
       public:
-      template <typename MeasureType> measure(bool, MeasureType &&m, bool enable_timer = true) : enable_timer(enable_timer) {
+      template <typename MeasureType> measure(bool, MeasureType &&m, bool enable_timer) : enable_timer(enable_timer) {
         static_assert(std::is_move_constructible<MeasureType>::value, "This measure is not MoveConstructible");
         static_assert(has_accumulate<MCSignType, MeasureType>::value, " This measure has no accumulate method !");
         static_assert(has_collect_result<MeasureType>::value, " This measure has no collect_results method !");
@@ -107,11 +107,11 @@ namespace triqs {
       /**
     * Register the Measure M with a name
     */
-      template <typename MeasureType> measure_ptr_t insert(MeasureType &&M, std::string const &name) {
+      template <typename MeasureType> measure_ptr_t insert(MeasureType &&M, std::string const &name, bool enable_timer) {
         if (has(name)) TRIQS_RUNTIME_ERROR << "measure_set : insert : measure '" << name << "' already inserted";
         // workaround for all gcc
         // m_map.insert(std::make_pair(name, measure_type(true, std::forward<MeasureType>(M))));
-        auto iter_b = m_map.emplace(name, measure_type(true, std::forward<MeasureType>(M)));
+        auto iter_b = m_map.emplace(name, measure_type(true, std::forward<MeasureType>(M), enable_timer));
         return iter_b.first;
       }
 
