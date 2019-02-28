6eacea60db792b4d14786b3f0cc2cd384e5cffb7
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-02-28 15:05:10 -0500

    Adjust fundamental operator set to respect order in gf_struct #342
    -In acc with the type gf_struct_t=vector<pair<int,vec<int/str>>>
    that describes the Block Green function structure
    this commit adjusts the fundamental operator set to respect this
    block-order
    -Contains unfixed test failures for atom_diag

--- triqs/hilbert_space/fundamental_operator_set.hpp
+++ triqs/hilbert_space/fundamental_operator_set.hpp
@@ -24,6 +24,7 @@
 #include <triqs/utility/dressed_iterator.hpp>
 #include <triqs/utility/exceptions.hpp>
 #include <triqs/utility/variant_extensions.hpp>
+#include <triqs/utility/itertools.hpp>
 #include <triqs/h5/base_public.hpp>
 #include <vector>
 #include <set>
@@ -32,8 +33,11 @@
 namespace triqs {
   namespace hilbert_space {
 
-    /// Structure of the Green's function
-    using gf_struct_t = std::vector<std::pair<std::string, std::vector<std::variant<int, std::string>>>>;
+    /// The index type of an operator
+    using indices_t = std::vector<std::variant<int, std::string>>;
+
+    /// Type type describing the structure of a Block Green's function
+    using gf_struct_t = std::vector<std::pair<std::string, indices_t>>;
 
     /// This class represents an ordered set of **indices** of the canonical operators (see [[many_body_operator]]) used to build the Fock states.
     /**
@@ -44,14 +48,13 @@ namespace triqs {
     class fundamental_operator_set {
       public:
       /// Sequence of indices (`std::vector` of int/string variant objects)
-      using indices_t = std::vector<std::variant<int, std::string>>;
+      using indices_t = triqs::hilbert_space::indices_t;
 
-      /// The set represented as a plain `std::vector`
+      /// The basic container `std::vector<indices_t>`
       using reduction_t = std::vector<indices_t>;
 
       private:
-      using map_t = std::map<indices_t, int>; // the table index <-> n
-      map_t map_index_n;
+      reduction_t vec = {};
 
       // internal only
       fundamental_operator_set(std::vector<std::vector<std::string>> const &);
@@ -62,8 +65,8 @@ namespace triqs {
 
       /// Construct a set with each stored index being a pair of integers `(i,j)`
       /**
-   @param v `i` runs from 0 to `v.size()-1`; `j` runs from 0 to `v[i].size()-1` for each `i`
-  */
+       * @param v `i` runs from 0 to `v.size()-1`; `j` runs from 0 to `v[i].size()-1` for each `i`
+       */
       fundamental_operator_set(std::vector<int> const &v) {
         for (int i = 0; i < v.size(); ++i)
           for (int j = 0; j < v[i]; ++j) insert(i, j);
@@ -71,130 +74,120 @@ namespace triqs {
 
       /// Construct from a set of generic index sequences
       /**
-   @param s Set of indices
-  */
+       * @param s Set of indices
+       */
       template <typename IndexType> fundamental_operator_set(std::set<IndexType> const &s) {
         for (auto const &i : s) insert(i);
       }
 
       /// Construct from a vector of index sequences
       /**
-   @param v Vector of indices
-  */
-      explicit fundamental_operator_set(reduction_t const &v) {
-        for (auto const &i : v) insert_from_indices_t(i);
-      }
+       * @param v Vector of indices
+       */
+      explicit fundamental_operator_set(reduction_t const &v): vec(v) {}
 
       /// Construct fundamental_operator_set on a GF structure
       /**
-   @param gf_struct GF structure object
-  */
+       * @param gf_struct GF structure object
+       */
       fundamental_operator_set(gf_struct_t const &gf_struct) {
         for (auto const &block : gf_struct)
           for (auto const &inner : block.second) insert(block.first, inner);
       }
 
       /// Reduce to a `std::vector<indices_t>`
-      explicit operator reduction_t() const { return reverse_map(); }
+      explicit operator reduction_t() const { return vec; }
 
       /// Insert a new index sequence given as `indices_t`
       /**
-   @param ind `indices_t` object
-  */
-      void insert_from_indices_t(indices_t const &ind) {
-        map_index_n.insert({ind, size()});
-        // reorder the indices, which are always given in the order of the indices tuple
-        map_t m;
-        int i = 0;
-        for (auto const &p : map_index_n) m.insert({p.first, i++});
-        std::swap(m, map_index_n);
-      }
+       * @param ind `indices_t` object
+       */
+      void insert_from_indices_t(indices_t const &ind) { if(!has_indices(ind)) vec.push_back(ind); }
 
       /// Insert a new index sequence given as multiple `int`/`std::string` arguments
       template <typename... IndexType> void insert(IndexType const &... ind) { insert_from_indices_t(indices_t{ind...}); }
 
       /// Number of elements in this set
       /**
-   @return Size of the set
-  */
-      int size() const { return map_index_n.size(); }
+       * @return Size of the set
+       */
+      int size() const { return vec.size(); }
 
       /// Check if a given index sequence is in this set
       /**
-   @param t Index sequence to look up
-   @return `true` if `t` is in this set
-  */
-      bool has_indices(indices_t const &t) const { return map_index_n.count(t) == 1; }
+       * @param t Index sequence to look up
+       * @return `true` if `t` is in this set
+       */
+      bool has_indices(indices_t const &t) const { return std::find(vec.begin(), vec.end(), t) != vec.end(); }
 
       /// Request position of a given index sequence
       /**
-   @param t Index sequence to look up
-   @return Position of the requested index sequence
-  */
+       * @param t Index sequence to look up
+       * @return Position of the requested index sequence
+       */
       int operator[](indices_t const &t) const {
-        try {
-          return map_index_n.at(t);
-        } catch (std::out_of_range &) { TRIQS_RUNTIME_ERROR << "Operator with indices (" << t << ") does not belong to this fundamental set!"; }
+	auto it = std::find(vec.begin(), vec.end(), t);
+	if(it == vec.end()) TRIQS_RUNTIME_ERROR << "Operator with indices (" << t << ") does not belong to this fundamental set!";
+	return std::distance(vec.begin(), it);
       }
 
       /// Comparison with another fundamental operator set
-      bool operator==(fundamental_operator_set const &fops) const { return map_index_n == fops.map_index_n; }
+      bool operator==(fundamental_operator_set const &fops) const { return vec == fops.vec; }
 
       /// Build and return the reverse map: `int` -> `indices_t`
       /**
-   @return The reverse map
-  */
-      std::vector<indices_t> reverse_map() const {
-        std::vector<indices_t> r(size());
-        for (auto const &x : map_index_n) r[x.second] = x.first;
-        return r;
+       * @return The reverse map
+       */
+      reduction_t reverse_map() const {
+        return vec;
       }
 
       // Dereference type for const_iterator
       struct _cdress {
         indices_t const &index;
         int linear_index;
-        _cdress(typename map_t::const_iterator _it) : index(_it->first), linear_index(_it->second) {}
+	using base_const_iterator = triqs::utility::details::enum_iter<reduction_t::const_iterator>;
+        _cdress(base_const_iterator _it) : linear_index(std::get<0>(*_it)), index(std::get<1>(*_it)) {}
       };
 
       /// Constant bidirectional iterator over all stored index sequences. For an iterator `it`, `it->index` gives the `indices_t` object pointed by this iterator, and `it->linear_index` is its position in the set.
-      using const_iterator = triqs::utility::dressed_iterator<typename map_t::const_iterator, _cdress>;
+      using const_iterator = triqs::utility::dressed_iterator<_cdress::base_const_iterator, _cdress>;
 
       /// Return `const_iterator` to the first element of this set
       /**
-   @return Iterator to the first index sequence
-  */
-      const_iterator begin() const noexcept { return map_index_n.begin(); }
+       * @return Iterator to the first index sequence
+       */
+      const_iterator begin() const noexcept { return triqs::utility::enumerate(vec).begin(); }
       /// Return `const_iterator` to the past-the-end element of this set
       /**
-   @return Iterator to the past-the-end element
-  */
-      const_iterator end() const noexcept { return map_index_n.end(); }
+       * @return Iterator to the past-the-end element
+       */
+      const_iterator end() const noexcept { return triqs::utility::enumerate(vec).end(); }
       /// Equivalent to [[fundamental_operator_set_begin]]
       /**
-   @return Iterator to the first index sequence
-  */
-      const_iterator cbegin() const noexcept { return map_index_n.cbegin(); }
+       * @return Iterator to the first index sequence
+       */
+      const_iterator cbegin() const noexcept { return triqs::utility::enumerate(vec).cbegin(); }
       /// Equivalent to [[fundamental_operator_set_end]]
       /**
-   @return Iterator to the past-the-end element
-  */
-      const_iterator cend() const noexcept { return map_index_n.cend(); }
+       * @return Iterator to the past-the-end element
+       */
+      const_iterator cend() const noexcept { return triqs::utility::enumerate(vec).cend(); }
 
       /// Write this set as an HDF5 attribute
       /**
-   @param id ID of an HDF5 object to attach the attribute to
-   @param name Name of the attribute
-   @param f Fundamental set to write
-  */
+       * @param id ID of an HDF5 object to attach the attribute to
+       * @param name Name of the attribute
+       * @param f Fundamental set to write
+       */
 
       friend void h5_write_attribute(hid_t id, std::string const &name, fundamental_operator_set const &f);
       /// Read a set from an HDF5 attribute
       /**
-   @param id ID of an HDF5 object the attribute is attached to
-   @param name Name of the attribute
-   @param f Reference to a fundamental set to be read
-  */
+       * @param id ID of an HDF5 object the attribute is attached to
+       * @param name Name of the attribute
+       * @param f Reference to a fundamental set to be read
+       */
       friend void h5_read_attribute(hid_t id, std::string const &name, fundamental_operator_set &f);
     };
   } // namespace hilbert_space
