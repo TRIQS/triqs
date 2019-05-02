11472ca06541907650dcc160529771e65ff8cb79
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-05-02 14:03:36 -0400

    [arrays] Clean memcopy function

--- triqs/arrays/storages/memcopy.hpp
+++ triqs/arrays/storages/memcopy.hpp
@@ -4,18 +4,13 @@
 
 namespace nda::mem {
 
-  template <class S> constexpr bool is_scalar_or_pod() {
-    return std::is_arithmetic<S>::value || triqs::is_complex<S>::value || std::is_pod<S>::value;
-  }
-
-  // ---------------------------
-
+  // FIXME : This is only for assignment. Move it.
   template <typename T1, typename T2> void memcopy(T1 * p1, const T2 * p2, size_t size) {
 
-    constexpr bool use_memcpy = is_scalar_or_pod<T1>() and std::is_same_v<T1, T2>;
+    constexpr bool use_memcpy = std::is_trivially_copyable_v<T1> and std::is_same_v<T1, T2>;
 
     if constexpr (use_memcpy) {
-      if (std::abs(p2 - p1) > size) { // guard against aliasing of data
+      if (std::abs(p2 - p1) > size) { // guard against overlapping of data
 	 std::memcpy(p1, p2, size * sizeof(T1));
         return;
       }
