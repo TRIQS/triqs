e27ac03c3461e4c089c2cf33bd1a07679707cef7
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-05-02 12:06:50 -0400

    Squashed 'cpp2py/' changes from 3ce45584..174e0282
    174e0282 [cmake] Properly add numpy 1.7.0 compat definition to interface of python_and_numpy target
    ab1c872c Fix raw string, \n and co
    956b4aeb Fix doc generation
    
    git-subtree-dir: cpp2py
    git-subtree-split: 174e0282de55203b2f1308afe5011bd295842c08

--- cpp2py/doc.py
+++ cpp2py/doc.py
@@ -5,32 +5,9 @@
 import re
 import cpp2py.clang_parser as CL
 import util
+from processed_doc import ProcessedDoc, replace_latex, clean_doc_string
 
-def replace_latex(s, escape_slash=False):
-    """replace 
-       $XX X$  by :math:`XX X`
-       $$X XX$$  by \n\n.. math:\n\t\tX XX\n\n..\n
-       [[ XXX]]  by :ref:` XXX`
-     
-     """
-    any_math_char = 'A-Za-z0-9{}\[\],;|\(\)=./\/+-_^\'' #any math character
-    #matches all expressions starting and ending with any math char, with possibly whitespaces in between
-    pattern_1 = '\$(['+any_math_char+']['+any_math_char+' ]*['+any_math_char+']+)\$'
-    #matches any single math char
-    pattern_2 = '\$(['+any_math_char+'])\$'
-    #out of line formula
-    text=re.sub('\$'+pattern_1+'\$', r'\n\n.. math::\n\t\t\1\n\n..\n', s)
-    text=re.sub('\$'+pattern_2+'\$', r'\n\n.. math::\n\t\t\1\n\n..\n', text)
-    #inline formula
-    text=re.sub(pattern_1, r':math:`\1`', text)
-    text=re.sub(pattern_2, r':math:`\1`', text)
-    #to create a hyperlink
-    text=re.sub('\[\[([A-Za-z0-9{}\(,\)=./\/+-_]+)\]\]', r':ref:`\1`', text)
-
-    if escape_slash: text=text.encode('string_escape')
-    return text
-
-def make_table(list_of_list):
+def make_table(*list_of_list):
     """
     :param list_of_list: list of list of strings
     :returns: a valid rst table
@@ -44,24 +21,6 @@ def make_table(list_of_list):
     for li in list_of_list: r += [form.format(*li), sep] 
     return '\n'.join(r) + '\n'
 
-def process_doc(doc):
-    if not doc : return ""
-    for p in ["/\*","\*/","^\s*\*", "///", "//", r"\\brief"] : 
-        doc = re.sub(p,"",doc,flags = re.MULTILINE)
-    doc = doc.strip()
-    doc = replace_latex(doc, True)
-    return doc
-
-
-def replace_cpp_keywords_by_py_keywords(s):
-    """replace syntax 
-       @param XXX blabla 
-       by
-       :param XXX: blabla
-    """
-    s=re.sub('@param ([A-Za-z0-9_]*) ',r':param \1: ', s)
-    return s
-
 def treat_member_list(member_list) : 
     class _m:
       def __init__(self, m):
@@ -70,7 +29,7 @@ def treat_member_list(member_list) :
     member_list2 = []
     for m in member_list:
       mm = _m(m)
-      mm.doc, doc_lines = "", [l.lstrip() for l in process_doc(m.raw_comment).splitlines()]
+      mm.doc, doc_lines = "", [l.lstrip() for l in clean_doc_string(m.raw_comment).splitlines()]
 
       #print m, doc_lines
       mm.initializer = CL.get_member_initializer(m)
@@ -89,23 +48,13 @@ def treat_member_list(member_list) :
       member_list2.append(mm)
     return member_list2
 
-def doc_format_param(member_list):
-   member_list2 = treat_member_list(member_list)
-   h = ['Parameter Name','Type','Default', 'Documentation']
-   l = [(m.spelling,m.ctype, m.initializer.replace("res.",""), m.doc) for m in member_list2]
-   return make_table(h, l)
+# FIXME : Obsolete. This is used for generate an rst file which is redundant.
+def doc_param_dict_format(member_list) :
+    """
+       member_list : list of the node of the members of a struct
 
-def make_doc(node):
-    """ process doc of node"""
-    
-    doc = process_doc(node.raw_comment)
-    if util.use_parameter_class(node):
-        doc = doc + '\n' + doc_format_param(CL.get_members(util.get_decl_param_class(node), True))
-    
-    return replace_latex(replace_cpp_keywords_by_py_keywords(doc))
-    
-
-def doc_param_dict_format(member_list) : 
+       returns the rst table
+    """
     member_list2 = treat_member_list(member_list)
     h= ['Parameter Name', 'Type', 'Default', 'Documentation']
     n_lmax = max(len(h[0]), max(len(m.spelling) for m in member_list2))
@@ -122,5 +71,52 @@ def doc_param_dict_format(member_list) :
     r = '\n'.join(lines)
     return sep2 + '\n' + header +'\n' + sep1 + '\n' + r 
 
+def decal(s, shift = 5):
+    sep = shift * ' '
+    return '\n'.join( (sep + x.strip()) for x in s.split('\n'))
+
+def make_doc_function(node):
+    """ Makes the doc of the node node"""
+    pdoc = ProcessedDoc(node) # general treatment and analysis, common with cpp2rst
+
+    # first case : it is a dict function
+    # FIXME : to be improved, what do we want ?
+    if util.use_parameter_class(node):
+        member_list = CL.get_members(util.get_decl_param_class(node), True)
+        table = doc_param_dict_format(member_list)
+        return "%s\n\n%s\n\n%s\n"%( pdoc.brief_doc, pdoc.doc, table) 
+
+        # member_list2 = treat_member_list(member_list)
+        # h = ['Parameter Name','Type','Default', 'Documentation']
+        # l = [(m.spelling,m.ctype, m.initializer.replace("res.",""), m.doc) for m in member_list2]
+        # table =  make_table(h, *l)
+        # doc = clean_doc_string(node.raw_comment) + '\n' + table
+        # return doc
+    
+    # General case
+
+    doc = "\n%s\n%s"%(pdoc.brief_doc, pdoc.doc)
+    doc = doc.strip() + "\n"
+   
+    params = pdoc.elements.pop('param', None) # parameters of the function
+    if params:
+       doc += "\nParameters\n----------\n"
+       for p in params:
+           name, comment = (p + '  ').split(' ',1)
+           doc += "%s \n%s\n\n"%(name, decal(comment))
+
+    ret = pdoc.elements.pop('return', None)
+    if ret:
+        _type = '' # FIXME : deduce the type ?
+        doc += "Returns\n-------\nout  %s\n%s\n\n"%(_type, decal(ret))
+   
+    return doc.strip()
+
+def make_doc_class(node):
+
+    # FIXME
+    return make_doc_function(node)
 
 
+def make_doc(node) : 
+    return clean_doc_string(node.raw_comment)
