50aef9c963a585a70f00f9ae82bf5e3dd4f59ae1
Nils Wentzell <nils.wentzell@gmail.com>
2019-05-10 21:24:54 -0600

    Add test for nda_handle clef heap-use-after-free

---
+++ test/triqs/gfs/clef_handle_bug.cpp
@@ -0,0 +1,25 @@
+#include <triqs/gfs.hpp>
+
+using namespace triqs::gfs;
+
+triqs::clef::placeholder<0> iw_;
+triqs::clef::placeholder<1> inu_;
+
+int nw      = 3;
+double beta = 20;
+
+int main() {
+
+  // Meshes
+  auto iw_mesh = gf_mesh<imfreq>{beta, Fermion, nw};
+  auto iw_inu_mesh = gf_mesh{iw_mesh, iw_mesh};
+
+  // -- Product Gf
+  auto Gp = gf{iw_inu_mesh, {1, 1}};
+  auto Gp1 = gf{Gp};
+
+  // Before commit 68b1d85e the following line was creating a copy
+  // of the underlying storage which was immediately freed in the slicing process
+  // This lead to Heap use after free (ASAN)
+  Gp1(iw_, inu_) << Gp(iw_, inu_)(0, 0);
+}
