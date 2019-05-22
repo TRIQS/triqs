19ffb03597a9ec44232781e0d1cb3d1554b4698f
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-05-22 19:43:12 -0400

    [mpi] Add mini doc for environment
    - To have it in the generated doc

--- mpi/mpi.hpp
+++ mpi/mpi.hpp
@@ -38,6 +38,7 @@ namespace mpi {
 
   // ------------------------------------------------------------
 
+  /// Environment must be initialized in C++
   struct environment {
 
     // MPICH does not allow Init without argc, argv, so we do not allow default constructors
