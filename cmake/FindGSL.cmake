1823300eb2fb2a3462a81dc26cf4a750885061d2
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-07-03 10:10:18 -0400

    [cmake] Synchronize and clean find_package modules for FFTW GMP GSL and NFFT

--- cmake/FindGSL.cmake
+++ cmake/FindGSL.cmake
@@ -1,46 +1,56 @@
 #  Copyright Olivier Parcollet 2010.
+#  Copyright Simons Foundation 2019
+#    Author: Nils Wentzell
+
 #  Distributed under the Boost Software License, Version 1.0.
 #      (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
 
 #
-# This module looks for fftw.
+# This module looks for gsl.
 # It sets up : GSL_INCLUDE_DIR, GSL_LIBRARIES
-# 
+# Use GSL_ROOT to specify a particular location
+#
 
-SET(TRIAL_PATHS
- $ENV{GSL_ROOT}/include
- ${GSL_ROOT}/include
- ENV CPATH
- ENV C_INCLUDE_PATH
- ENV CPLUS_INCLUDE_PATH
- ENV OBJC_INCLUDE_PATH
- ENV OBJCPLUS_INCLUDE_PATH
- /usr/include
- /usr/local/include
- /opt/local/include
- /sw/include
- )
-FIND_PATH(GSL_INCLUDE_DIR gsl/gsl_math.h ${TRIAL_PATHS} DOC "Include for GSL")
+find_path(GSL_INCLUDE_DIR
+  NAMES gsl_math.h
+  PATHS
+    ${GSL_ROOT}/include
+    $ENV{GSL_ROOT}/include
+    ENV CPATH
+    ENV C_INCLUDE_PATH
+    ENV CPLUS_INCLUDE_PATH
+    ENV OBJC_INCLUDE_PATH
+    ENV OBJCPLUS_INCLUDE_PATH
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    /sw/include
+  DOC "Include Directory for GSL"
+)
 
-SET(TRIAL_LIBRARY_PATHS
- $ENV{GSL_ROOT}/lib
- ${GSL_ROOT}/lib
- ${GSL_INCLUDE_DIR}/../lib
- ENV LIBRARY_PATH
- ENV LD_LIBRARY_PATH
- /usr/lib 
- /usr/local/lib
- /opt/local/lib
- /sw/lib
- )
+find_library(GSL_LIBRARIES
+  NAMES gsl libgsl
+  PATHS
+    ${GSL_INCLUDE_DIR}/../lib
+    ${GSL_ROOT}/lib
+    $ENV{GSL_ROOT}/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+  DOC "GSL library"
+)
 
-SET(GSL_LIBRARIES "GSL_LIBRARIES-NOTFOUND" CACHE STRING "GSL library")
-# Try to detect the lib
-FIND_LIBRARY(GSL_LIBRARIES gsl ${TRIAL_LIBRARY_PATHS} DOC "GSL library")
-
-mark_as_advanced(GSL_INCLUDE_DIR)
-mark_as_advanced(GSL_LIBRARIES)
-
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GSL DEFAULT_MSG GSL_LIBRARIES GSL_INCLUDE_DIR)
 
+mark_as_advanced(GSL_INCLUDE_DIR GSL_LIBRARIES)
+
+# Interface target
+# We refrain from creating an imported target since those cannot be exported
+add_library(gsl INTERFACE)
+target_link_libraries(gsl INTERFACE ${GSL_LIBRARIES})
+target_include_directories(gsl SYSTEM INTERFACE ${GSL_INCLUDE_DIR})
--- cmake/FindNFFT.cmake
+++ cmake/FindNFFT.cmake
@@ -1,61 +1,69 @@
-#  Copyright Olivier Parcollet 2014.
+#  Copyright Olivier Parcollet 2014
+#  Copyright Simons Foundation 2019
+#    Author: Nils Wentzell
+
 #  Distributed under the Boost Software License, Version 1.0.
 #      (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
 
 #
 # This module looks for nfft.
-# It sets up : NFFT_INCLUDE_DIR, NFFT_LIBRARY, NFFT_OLD_API
-# 
+# It sets up : NFFT_INCLUDE_DIR, NFFT_LIBRARIES, NFFT_OLD_API
+# Use NFFT_ROOT to specify a particular location
+#
 
-INCLUDE( FindPackageHandleStandardArgs ) 
+if(NFFT_INCLUDE_DIR AND NFFT_LIBRARIES)
+  set(NFFT_FIND_QUIETLY TRUE)
+endif()
 
-SET(TRIAL_PATHS
- $ENV{NFFT_ROOT}/include
- ${NFFT_ROOT}/include
- ENV CPATH
- ENV C_INCLUDE_PATH
- ENV CPLUS_INCLUDE_PATH
- ENV OBJC_INCLUDE_PATH
- ENV OBJCPLUS_INCLUDE_PATH
- /usr/include
- /usr/local/include
- /opt/local/include
- /sw/include
- )
-FIND_PATH(NFFT_INCLUDE_DIR nfft3.h ${TRIAL_PATHS} DOC "Include for NFFT")
+find_path(NFFT_INCLUDE_DIR
+  NAMES nfft3.h
+  PATHS
+    ${NFFT3_ROOT}/include
+    ${NFFT_ROOT}/include
+    $ENV{NFFT3_ROOT}/include
+    $ENV{NFFT_ROOT}/include
+    ENV CPATH
+    ENV C_INCLUDE_PATH
+    ENV CPLUS_INCLUDE_PATH
+    ENV OBJC_INCLUDE_PATH
+    ENV OBJCPLUS_INCLUDE_PATH
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    /sw/include
+  DOC "Include Directory for NFFT"
+)
 
-SET(TRIAL_LIBRARY_PATHS
- $ENV{NFFT_ROOT}/lib
- ${NFFT_ROOT}/lib
- ${NFFT_INCLUDE_DIR}/../lib
- ENV LIBRARY_PATH
- ENV LD_INCLUDE_PATH
- /usr/lib 
- /usr/lib/x86_64-linux-gnu
- /usr/local/lib
- /opt/local/lib
- /sw/lib
- )
+find_library(NFFT_LIBRARIES
+  NAMES nfft3
+  PATHS
+    ${NFFT_INCLUDE_DIR}/../lib
+    ${NFFT3_ROOT}/lib
+    ${NFFT_ROOT}/lib
+    $ENV{NFFT3_ROOT}/lib
+    $ENV{NFFT_ROOT}/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+  DOC "NFFT library"
+)
 
-SET(NFFT_LIBRARY "NFFT_LIBRARY-NOTFOUND" CACHE STRING "NFFT library")
-# Try to detect the lib
-FIND_LIBRARY(NFFT_LIBRARY nfft3 ${TRIAL_LIBRARY_PATHS} DOC "NFFT library")
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(NFFT DEFAULT_MSG NFFT_LIBRARIES NFFT_INCLUDE_DIR)
 
-mark_as_advanced(NFFT_INCLUDE_DIR)
-mark_as_advanced(NFFT_LIBRARY)
+mark_as_advanced(NFFT_INCLUDE_DIR NFFT_LIBRARIES)
 
 file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/nfft_api_test.cpp "#include \"nfft3.h\"\nint main(){ nfft_plan p; return p.nfft_flags; }")
 try_compile(NFFT_OLD_API ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR}/nfft_api_test.cpp COMPILE_DEFINITIONS -I${NFFT_INCLUDE_DIR} )
 file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/a.out ${CMAKE_CURRENT_BINARY_DIR}/nfft_api_test.cpp)
 
-# Imported target
-
-add_library(nfft UNKNOWN IMPORTED)
-set_property(TARGET nfft PROPERTY IMPORTED_LOCATION ${NFFT_LIBRARY})
-set_property(TARGET nfft PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${NFFT_INCLUDE_DIR})
-set_property(TARGET nfft PROPERTY PROPERTY INTERFACE_COMPILE_DEFINITIONS $<$<BOOL:${NFFT_OLD_API}>:NFFT_OLD_API>)
-
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(NFFT DEFAULT_MSG NFFT_LIBRARY NFFT_INCLUDE_DIR)
-
-
+# Interface target
+# We refrain from creating an imported target since those cannot be exported
+add_library(nfft INTERFACE)
+target_link_libraries(nfft INTERFACE ${NFFT_LIBRARIES})
+target_include_directories(nfft SYSTEM INTERFACE ${NFFT_INCLUDE_DIR})
+target_compile_definitions(nfft INTERFACE $<$<BOOL:${NFFT_OLD_API}>:NFFT_OLD_API>)
