c47969e59fb6ec7bbd808577549862e1019908ba
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-11-20 16:31:14 -0500

    Update GfFill benchmark after nda and mesh changes

--- benchmarks/GfFill.cpp
+++ benchmarks/GfFill.cpp
@@ -1,8 +1,10 @@
 #include <benchmark/benchmark.h>
 #include <triqs/gfs.hpp>
+#include <triqs/mesh.hpp>
+#include <nda/nda.hpp>
 
-using namespace triqs::arrays;
 using namespace triqs::gfs;
+using namespace triqs::mesh;
 
 using scalar_t = std::complex<double>;
 
@@ -10,13 +12,13 @@ using scalar_t = std::complex<double>;
 
 static void ArrayFill(benchmark::State &state) {
   std::size_t n_iw = state.range(0);
-  array<scalar_t, 2> arr{4 * n_iw, 4 * n_iw};
-  array<scalar_t, 3> tensor{2 * n_iw + 1, 2 * n_iw, 2 * n_iw};
+  nda::array<scalar_t, 2> arr{4 * n_iw, 4 * n_iw};
+  nda::array<scalar_t, 3> tensor{2 * n_iw + 1, 2 * n_iw, 2 * n_iw};
   for (auto _ : state) {
     for (int q : range(-n_iw, n_iw + 1)) // Bosonic
       for (int m : range(-n_iw, n_iw))   // Fermionic
-        for (int n : range(-n_iw, n_iw)) // Fermionic
-          tensor(q + n_iw, m + n_iw, n + n_iw) += arr(m + 2 * n_iw, q + m + 2 * n_iw) * arr(q + n + 2 * n_iw, n + 2 * n_iw);
+	for (int n : range(-n_iw, n_iw)) // Fermionic
+	  tensor(q + n_iw, m + n_iw, n + n_iw) += arr(m + 2 * n_iw, q + m + 2 * n_iw) * arr(q + n + 2 * n_iw, n + 2 * n_iw);
   }
   state.SetBytesProcessed(int64_t(state.iterations()) * 8 * n_iw * n_iw * n_iw * sizeof(scalar_t));
 }
@@ -29,11 +31,11 @@ static void GfFillLoop(benchmark::State &state) {
   long n_iw = state.range(0);
 
   auto iw_mesh_large = gf_mesh<imfreq>{1.0, Fermion, 2 * n_iw};
-  auto M             = gf<cartesian_product<imfreq, imfreq>, scalar_valued>{{iw_mesh_large, iw_mesh_large}};
+  auto M             = gf<prod<imfreq, imfreq>, scalar_valued>{{iw_mesh_large, iw_mesh_large}};
 
   auto iw_mesh = gf_mesh<imfreq>{1.0, Fermion, n_iw};
   auto iW_mesh = gf_mesh<imfreq>{1.0, Boson, n_iw};
-  auto M4      = gf<cartesian_product<imfreq, imfreq, imfreq>, scalar_valued>{{iW_mesh, iw_mesh, iw_mesh}};
+  auto M4      = gf<prod<imfreq, imfreq, imfreq>, scalar_valued>{{iW_mesh, iw_mesh, iw_mesh}};
 
   for (auto _ : state)
     for (auto iW : iW_mesh)
@@ -56,11 +58,11 @@ static void GfFillClef(benchmark::State &state) {
   triqs::clef::placeholder_prime<2> iw2;
 
   auto iw_mesh_large = gf_mesh<imfreq>{1.0, Fermion, 2 * n_iw};
-  auto M             = gf<cartesian_product<imfreq, imfreq>, scalar_valued>{{iw_mesh_large, iw_mesh_large}};
+  auto M             = gf<prod<imfreq, imfreq>, scalar_valued>{{iw_mesh_large, iw_mesh_large}};
 
   auto iw_mesh = gf_mesh<imfreq>{1.0, Fermion, n_iw};
   auto iW_mesh = gf_mesh<imfreq>{1.0, Boson, n_iw};
-  auto M4      = gf<cartesian_product<imfreq, imfreq, imfreq>, scalar_valued>{{iW_mesh, iw_mesh, iw_mesh}};
+  auto M4      = gf<prod<imfreq, imfreq, imfreq>, scalar_valued>{{iW_mesh, iw_mesh, iw_mesh}};
 
   //for (auto _ : state) M4[iW, iw1_, iw2_] << M4[iW, iw1_, iw2_] + M[iw1_, iW + iw1_] * M[iW + iw2_, iw2_]; FIXME Does not work!
   for (auto _ : state) M4(iW, iw1, iw2) << M4(iW, iw1, iw2) + M(iw1, iW + iw1) * M(iW + iw2, iw2);
