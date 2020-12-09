dd1a866f6adcdb7e2d083a1715865f0902f16e8e
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-12-09 15:12:59 -0500

    Adjust to renaming data_start -> data in nda

--- c++/triqs/gfs/transform/fourier_common.cpp
+++ c++/triqs/gfs/transform/fourier_common.cpp
@@ -24,8 +24,8 @@ namespace triqs::gfs {
 
   void _fourier_base(array_const_view<dcomplex, 2> in, array_view<dcomplex, 2> out, int rank, int *dims, int fftw_count, int fftw_backward_forward) {
 
-    auto in_fft  = reinterpret_cast<fftw_complex *>(const_cast<dcomplex*>(in.data_start()));
-    auto out_fft = reinterpret_cast<fftw_complex *>(out.data_start());
+    auto in_fft  = reinterpret_cast<fftw_complex *>(const_cast<dcomplex*>(in.data()));
+    auto out_fft = reinterpret_cast<fftw_complex *>(out.data());
 
     auto p = fftw_plan_many_dft(rank,                        // rank
                                 dims,                        // the dimension
@@ -46,8 +46,8 @@ namespace triqs::gfs {
 
   //void _fourier_base(array_const_view<double, 2> in, array_view<dcomplex, 2> out, int rank, int *dims, int fftw_count) {
 
-    //auto in_fft  = reinterpret_cast<fftw_real *>(in.data_start());
-    //auto out_fft = reinterpret_cast<fftw_complex *>(out.data_start());
+    //auto in_fft  = reinterpret_cast<fftw_real *>(in.data());
+    //auto out_fft = reinterpret_cast<fftw_complex *>(out.data());
 
     //auto p = fftw_plan_many_dft(rank,                        // rank
                                 //dims,                        // the dimension
