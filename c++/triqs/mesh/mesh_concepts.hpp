fccc828f69f80b3831e04c470381844c80d1483e
Philipp Dumitrescu <pdumitrescu@flatironinstitute.org>
2021-11-29 16:31:25 -0500

    [mesh] change mesh concept
    * domain_t added back
    * mesh_pt_t reintroduced, due to mesh inheritance
    * use ifdef as clang-13 is missing ranges implementation

--- c++/triqs/mesh/mesh_concepts.hpp
+++ c++/triqs/mesh/mesh_concepts.hpp
@@ -60,33 +60,44 @@ namespace triqs::mesh {
   // A mesh is a domain and a iterator ("std::range") over a subset of that domain (which may be the full domain).
   // Mesh must be h5 serializable and hashable (to quickly check if a mesh point originates from a mesh)
   template <typename M>
-  concept Mesh = std::regular<M> && H5Serializable<M> && Hashable<M> && requires(M const &m) {
+  concept Mesh = std::regular<M> && H5Serializable<M> && requires(M const &m) {
     // Mesh has a domain
-    { m.domain() } -> Domain;
+    requires Domain<std::remove_cvref_t<decltype(m.domain())>>;
+    typename M::domain_t;
 
+    // mesh_point_t does not need to be mesh_point<M>, because of inheritance returning iter with mesh_point<Base>
+    typename M::mesh_point_t;
+
+// libc++ does not yet fully implement ranges for now: use itertools
+#if defined(_LIBCPP_VERSION) and (__clang_major__ < 14)
+    // requires std::forward_iterator<std::ranges::iterator_t<M>>;  // Currently itertools::transform is not constructible
+    {m.size()};
+#else
     // Has linear index and a range which iterates over mesh points
     requires std::ranges::random_access_range<M>;
     requires std::ranges::sized_range<M>;
-    { *std::ranges::cbegin(m) } -> std::same_as<mesh_point<M>>;
+#endif
+
+    { *std::begin(m) } -> std::same_as<typename M::mesh_point_t>;
 
     // Has linear_index and index as well as a bijection between them
     requires std::same_as<typename M::linear_index_t, long>;
     typename M::index_t;
 
   } && requires(M const &m, typename M::index_t const &idx) {
-    { m.index_to_linear_index(idx) } -> std::same_as<typename M::linear_index_t>; // naming!
-    { m.operator[](idx) } -> std::same_as<mesh_point<M>>;
-  } && requires(M const &m, typename M::idx_linear const &idx_lin) {
-    { m.linear_index_to_index(idx_lin) } -> std::same_as<typename M::index_t>; // naming!
-    { m.linear_index_to_mesh_pt(idx_lin) } -> std::same_as<mesh_point<M>>;     // naming!
+    { m.index_to_linear_index(idx) } -> std::same_as<typename M::linear_index_t>; // TODO: naming!
+    { m.operator[](idx) } -> std::same_as<typename M::mesh_point_t>;
+  } && requires(M const &m, typename M::linear_index_t const &idx_lin) {
+    { m.linear_index_to_index(idx_lin) } -> std::same_as<typename M::index_t>;        // TODO:  naming!
+    { m.linear_index_to_mesh_pt(idx_lin) } -> std::same_as<typename M::mesh_point_t>; // TODO: naming!
   };
 
-  template <template <typename> class P, typename M>
-  concept MeshPoint = Mesh<M> && requires(P<M> const &mesh_point, M const &mesh) {
-    { mesh_point.index } -> std::same_as<typename M::index_t>;
-    { mesh_point.linear_index } -> std::same_as<typename M::linear_index_t>;
-    { mesh_point.value } -> std::same_as<typename decltype(mesh.domain())::point_t>;
-    { mesh_point.mesh_hash } -> std::convertible_to<std::size_t>;
+  template <typename M>
+  concept MeshPoint = Mesh<M> && requires(mesh_point<M> const &mp) {
+    { mp.index } -> std::same_as<typename M::index_t>;
+    { mp.linear_index } -> std::same_as<typename M::linear_index_t>;
+    { mp.value } -> std::same_as<typename M::domain_t::point_t>;
+    { mp.mesh_hash } -> std::convertible_to<std::size_t>;
   };
 
 } // namespace triqs::mesh
