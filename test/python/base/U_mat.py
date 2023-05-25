e672518393e6843f4a5ad29c6694b420894f92d2
Alexander Hampel <ahampel@flatironinstitute.org>
2023-05-25 05:52:23 -0400

    include @krivenko suggestions and add test

--- test/python/base/U_mat.py
+++ test/python/base/U_mat.py
@@ -23,12 +23,34 @@ from triqs.operators.util import *
 from triqs.utility.comparison_tests import *
 import numpy
 
+
+# test slater
 U_sph = U_matrix_slater(l=2, U_int=2.0, J_hund=0.5)
-U_cubic = transform_U_matrix(U_sph,spherical_to_cubic(l=2))
-U,Up = reduce_4index_to_2index(U_cubic)
+U_cubic = transform_U_matrix(U_sph, spherical_to_cubic(l=2))
+U, Up = reduce_4index_to_2index(U_cubic)
 
 with HDFArchive('U_mat.ref.h5', 'r') as ar:
    assert_arrays_are_close(ar['Ufull_sph'], U_sph)
    assert_arrays_are_close(ar['Ufull_cubic'], U_cubic)
    assert_arrays_are_close(ar['U'], U)
    assert_arrays_are_close(ar['Up'], Up)
+
+
+# test Kanamori
+U_kan_ijkl = U_matrix_kanamori(n_orb=5, U_int=3.5, J_hund=1.1, full_Uijkl=True)
+U_ref, U_p_ref = reduce_4index_to_2index(U_kan_ijkl)
+
+U, U_p = U_matrix_kanamori(n_orb=5, U_int=3.5, J_hund=1.1, full_Uijkl=False)
+
+assert_arrays_are_close(U_ref, U)
+assert_arrays_are_close(U_p_ref, U_p)
+
+
+# test Kanamori with different Up
+U_kan_ijkl = U_matrix_kanamori(n_orb=3, U_int=3.2, Up_int=1.67, J_hund=1.35, full_Uijkl=True)
+U_ref, U_p_ref = reduce_4index_to_2index(U_kan_ijkl)
+
+U, U_p = U_matrix_kanamori(n_orb=3, U_int=3.2, Up_int=1.67, J_hund=1.35, full_Uijkl=False)
+
+assert_arrays_are_close(U_ref, U)
+assert_arrays_are_close(U_p_ref, U_p)
