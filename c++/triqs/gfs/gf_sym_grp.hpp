40824dea0bd5ea4f59f9c0ba4a73748a7150a488
Nils Wentzell <nwentzell@flatironinstitute.org>
2023-08-09 15:34:35 -0400

    Minor simplifications and refactoring in gf_sym_grp implementation

--- c++/triqs/gfs/gf_sym_grp.hpp
+++ c++/triqs/gfs/gf_sym_grp.hpp
@@ -27,16 +27,16 @@ namespace triqs {
     template <typename F, typename G, typename mesh_index_t = typename G::mesh_t::index_t>
     concept ScalarGfSymmetry = is_gf_v<G> and //
        requires(F f, mesh_index_t const &mesh_index) {
-         requires(G::target_t::rank == 0);
+         requires(G::target_rank == 0);
          { f(mesh_index) } -> std::same_as<std::tuple<mesh_index_t, nda::operation>>;
        };
 
     // symmetry concept for tensor valued gf: symmetry accepts mesh + target index and returns new mesh + target index & operation
     template <typename F, typename G, typename mesh_index_t = typename G::mesh_t::index_t,
-              typename target_index_t = std::array<long, static_cast<std::size_t>(G::target_t::rank)>>
+              typename target_index_t = std::array<long, static_cast<std::size_t>(G::target_rank)>>
     concept TensorGfSymmetry = is_gf_v<G> and //
        requires(F f, mesh_index_t const &mesh_index, target_index_t const &target_index) {
-         requires(G::target_t::rank > 0);
+         requires(G::target_rank > 0);
          { f(mesh_index, target_index) } -> std::same_as<std::tuple<mesh_index_t, target_index_t, nda::operation>>;
        };
 
@@ -44,34 +44,34 @@ namespace triqs {
     template <typename F, typename G, typename mesh_index_t = typename G::mesh_t::index_t>
     concept ScalarGfInitFunc = is_gf_v<G> and //
        requires(F f, mesh_index_t const &mesh_index) {
-         requires(G::target_t::rank == 0);
+         requires(G::target_rank == 0);
          { f(mesh_index) } -> std::same_as<typename G::scalar_t>;
        };
 
     // init function concept for tensor valued gf: init function accepts mesh index + target index and returns gf value type
     template <typename F, typename G, typename mesh_index_t = typename G::mesh_t::index_t,
-              typename target_index_t = std::array<long, static_cast<std::size_t>(G::target_t::rank)>>
+              typename target_index_t = std::array<long, static_cast<std::size_t>(G::target_rank)>>
     concept TensorGfInitFunc = is_gf_v<G> and //
        requires(F f, mesh_index_t const &mesh_index, target_index_t const &target_index) {
-         requires(G::target_t::rank > 0);
+         requires(G::target_rank > 0);
          { f(mesh_index, target_index) } -> std::same_as<typename G::scalar_t>;
        };
 
     // make tuple from array
-    template <typename arr_t> auto make_tuple_from_array(arr_t const &arr) {
+    template <typename A> auto to_tuple(A const &arr) {
       constexpr auto fetch = [](auto const &...xs) { return std::tuple{xs...}; };
       return std::apply(fetch, arr);
     }
 
     // make tuple from array using index_sequence
-    template <typename arr_t, std::size_t... Is> auto make_tuple_from_array(arr_t const &arr, std::index_sequence<Is...>) {
-      return std::tuple{arr[Is]...};
+    template <std::size_t R, typename A> auto to_tuple(A const &arr) {
+      return [&]<std::size_t... Is>(std::index_sequence<Is...>) { return std::tuple{arr[Is]...}; }(std::make_index_sequence<R>{});
     }
 
     // make array from tuple
-    template <typename tuple_t> auto make_array_from_tuple(tuple_t const &tuple) {
+    template <typename tpl_t> auto to_array(tpl_t const &tpl) {
       constexpr auto fetch = [](auto const &...xs) { return std::array{xs...}; };
-      return std::apply(fetch, tuple);
+      return std::apply(fetch, tpl);
     }
 
     template <typename F, typename G>
@@ -91,9 +91,8 @@ namespace triqs {
       static constexpr auto mesh_rank = n_variables<typename G::mesh_t>;
 
       // target aliases
-      using target_t                    = typename G::target_t;
-      static constexpr auto target_rank = target_t::rank;
-      using target_index_t              = std::array<long, static_cast<std::size_t>(target_rank)>;
+      static constexpr size_t target_rank = G::target_rank;
+      using target_index_t                = std::array<long, static_cast<std::size_t>(target_rank)>;
 
       // members
       nda::sym_grp<data_sym_func_t, data_t> data_sym_grp; // symmetry group instance for the data array
@@ -101,27 +100,24 @@ namespace triqs {
       // convert from gf to nda symmetry
       data_sym_func_t to_data_symmetry(F const &f, G const &g) const {
 
-        auto fp = [f, m = g.mesh()](data_index_t const &x) {
+        auto fp = [f, m = g.mesh()](data_index_t const &x) -> std::tuple<data_index_t, nda::operation> {
           // init new data index and residual operation
           data_index_t xp;
-          nda::operation op;
 
-          // scalar valued gfs
-          if constexpr (target_rank == 0) {
+          if constexpr (target_rank == 0) { // scalar valued gfs
 
             if constexpr (mesh_rank == 1) {
-              auto [new_mesh_index, opp] = f(m.to_index(x[0]));
-              xp[0]                      = m.to_data_index(new_mesh_index);
-              op                         = opp;
+              auto [new_mesh_index, op] = f(m.to_index(x[0]));
+              xp[0]                     = m.to_data_index(new_mesh_index);
+              return {xp, op};
 
-            } else {
-              auto [new_mesh_index, opp] = f(m.to_index(make_tuple_from_array(x)));
-              xp                         = make_array_from_tuple(m.to_data_index(new_mesh_index));
-              op                         = opp;
+            } else { // product mesh
+              auto [new_mesh_index, op] = f(m.to_index(to_tuple(x)));
+              xp                        = to_array(m.to_data_index(new_mesh_index));
+              return {xp, op};
             }
 
-            // tensor valued gfs
-          } else {
+          } else { // tensor valued gfs
 
             // convert data index to target index
             target_index_t target_index;
@@ -129,33 +125,32 @@ namespace triqs {
 
             if constexpr (mesh_rank == 1) {
               // evaluate symmetry
-              auto [new_mesh_index, new_target_index, opp] = f(m.to_index(x[0]), target_index);
+              auto [new_mesh_index, new_target_index, op] = f(m.to_index(x[0]), target_index);
 
               // convert mesh index + target index back to data index
               xp[0] = m.to_data_index(new_mesh_index);
               for (auto i : range(target_rank)) xp[i + mesh_rank] = new_target_index[i];
-              op = opp;
 
-            } else {
+              return {xp, op};
+
+            } else { // product mesh
               // evaluate symmetry
-              auto [new_mesh_index, new_target_index, opp] =
-                 f(m.to_index(make_tuple_from_array(x, std::make_index_sequence<mesh_rank>{})), target_index);
+              auto [new_mesh_index, new_target_index, op] = f(m.to_index(to_tuple<mesh_rank>(x)), target_index);
 
               // convert mesh index + target index back to data index
-              auto new_mesh_arr = make_array_from_tuple(m.to_data_index(new_mesh_index));
+              auto new_mesh_arr = to_array(m.to_data_index(new_mesh_index));
               for (auto i : range(mesh_rank)) xp[i] = new_mesh_arr[i];
               for (auto i : range(target_rank)) xp[i + mesh_rank] = new_target_index[i];
-              op = opp;
+
+              return {xp, op};
             }
           }
-
-          return std::tuple{xp, op};
         };
 
         return fp;
       };
 
-      // convert from list of gf to list of nda symmetry
+      // convert from list of gf symmetries to list of nda symmetries
       std::vector<data_sym_func_t> to_data_symmetry_list(G const &g, std::vector<F> const &sym_list) const {
         std::vector<data_sym_func_t> data_sym_list;
         for (auto f : sym_list) data_sym_list.push_back(to_data_symmetry(f, g));
@@ -166,18 +161,16 @@ namespace triqs {
       template <typename H> data_init_func_t to_data_init_func(G const &g, H const &h) const {
 
         auto hp = [h, m = g.mesh()](data_index_t const &x) {
-          // scalar valued gfs
-          if constexpr (target_rank == 0) {
+          if constexpr (target_rank == 0) { // scalar valued gfs
 
             if constexpr (mesh_rank == 1) {
               return h(m.to_index(x[0]));
 
-            } else {
-              return h(m.to_index(make_tuple_from_array(x)));
+            } else { // product mesh
+              return h(m.to_index(to_tuple(x)));
             }
 
-            // tensor valued gfs
-          } else {
+          } else { // tensor valued gfs
 
             target_index_t target_index;
             for (auto i : range(target_rank)) target_index[i] = x[i + mesh_rank];
@@ -185,8 +178,8 @@ namespace triqs {
             if constexpr (mesh_rank == 1) {
               return h(m.to_index(x[0]), target_index);
 
-            } else {
-              return h(m.to_index(make_tuple_from_array(x, std::make_index_sequence<mesh_rank>{})), target_index);
+            } else { // product mesh
+              return h(m.to_index(to_tuple<mesh_rank>(x)), target_index);
             }
           }
         };
@@ -196,7 +189,7 @@ namespace triqs {
 
       public:
       [[nodiscard]] nda::sym_grp<data_sym_func_t, data_t> const &get_data_sym_grp() const { return data_sym_grp; }
-      long num_classes() const { return data_sym_grp.num_classes(); }
+      [[nodiscard]] long num_classes() const { return data_sym_grp.num_classes(); }
 
       sym_grp() = default;
       sym_grp(G const &g, std::vector<F> const &sym_list, long const max_length = 0)
@@ -215,28 +208,26 @@ namespace triqs {
         auto const &[max_diff, max_index] = data_sym_grp.symmetrize(g.data());
         auto const m                      = g.mesh();
 
-        // scalar valued gfs
-        if constexpr (target_rank == 0) {
+        if constexpr (target_rank == 0) { // scalar valued gfs
 
           if constexpr (mesh_rank == 1) {
-            return std::tuple{max_diff, m.to_index(max_index[0]), target_index_t{}};
+            return {max_diff, m.to_index(max_index[0]), {}};
 
-          } else {
-            return std::tuple{max_diff, m.to_index(make_tuple_from_array(max_index)), target_index_t{}};
+          } else { // product mesh
+            return {max_diff, m.to_index(to_tuple(max_index)), {}};
           }
 
-          // tensor valued gfs
-        } else {
+        } else { // tensor valued gfs
 
           // convert data index to target index
           target_index_t target_index;
           for (auto i : range(target_rank)) target_index[i] = max_index[i + mesh_rank];
 
           if constexpr (mesh_rank == 1) {
-            return std::tuple{max_diff, m.to_index(max_index[0]), target_index};
+            return {max_diff, m.to_index(max_index[0]), target_index};
 
-          } else {
-            return std::tuple{max_diff, m.to_index(make_tuple_from_array(max_index, std::make_index_sequence<mesh_rank>{})), target_index};
+          } else { // product mesh
+            return {max_diff, m.to_index(to_tuple<mesh_rank>(max_index)), target_index};
           }
         }
       }
