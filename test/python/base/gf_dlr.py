a63586a64f86d867935ec7b58f4e737d5b29283b
Alexander Hampel <ahampel@flatironinstitute.org>
2023-08-10 13:30:10 -0400

    Update test/python/base/gf_dlr.py
    Co-authored-by: Nils Wentzell <Wentzell@users.noreply.github.com>

--- test/python/base/gf_dlr.py
+++ test/python/base/gf_dlr.py
@@ -147,7 +147,7 @@ class test_dlr_mesh(unittest.TestCase):
         g.data[:] = np.random.randn(len(m))
 
         # block gf test
-        Bgdlr = BlockGf(name_list=['1', '2'], block_list=[g, g], make_copies=True)
+        Bgdlr = BlockGf(block_list=[g, g], make_copies=True)
 
         for tau in np.linspace(0, beta, num=10):
             ref = np.sum(g.data * -np.exp(-rf * tau/beta) / ( 1 + np.exp(-rf) ))
