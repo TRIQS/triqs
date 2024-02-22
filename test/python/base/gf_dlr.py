Fix #933 - mesh::dlr_imfreq size function returned wrong value for bosonic symmetrized case

test/python/base/gf_dlr.py
@@ -27,9 +27,9 @@ from triqs.gf.meshes import MeshDLR
 
 from triqs.utility.comparison_tests import *
 
-def onefermion(tau, eps, beta):
+def onefermion(tau, omega, beta):
     from math import exp
-    return -exp(-eps * tau) / (1 + exp(-beta * eps));
+    return -exp(-omega * tau) / (1 + exp(-beta * omega));
 
 class test_dlr_mesh(unittest.TestCase):
 
@@ -201,6 +201,28 @@ class test_dlr_mesh(unittest.TestCase):
 
         for iwn in g.mesh:
             assert np.allclose(g[iwn],np.array([[3.,-1.],[0.,3.]]))
+
+    def test_dlr_symmetrized(self):
+
+        beta = 20
+        tau_mesh = MeshDLRImTime(beta = beta, statistic = 'Boson', w_max = 2.0, eps = 1e-15, symmetrize = True)
+
+        tau_values = np.fromiter(tau_mesh.values(), dtype=float)
+        assert(np.allclose(tau_values, beta - tau_values[::-1]))
+
+        gtau = Gf(mesh = tau_mesh, target_shape = [])
+        for tau in gtau.mesh:
+            gtau[tau] = onefermion(tau, omega = 1.1, beta = beta)
+
+        gdlr = make_gf_dlr(gtau)
+        giw  = make_gf_dlr_imfreq(gdlr)
+
+        iw_values = np.fromiter(giw.mesh.values(), dtype=complex)
+        assert(np.allclose(iw_values, -iw_values[::-1]))
+        assert(len(iw_values) % 2 == 1) # odd number of frequencies
+
+        assert_gfs_are_close(make_gf_dlr_imtime(make_gf_dlr(giw)), gtau)
+
 
 if __name__ == '__main__':
     unittest.main()