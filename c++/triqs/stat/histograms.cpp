183fad202a91f55cae155a63f887bbc391eb6ac1
Thomas Hahn <thomas.hahn01@gmail.com>
2024-10-29 11:56:48 -0400

    Clean up histograms.hpp/.cpp and update tests

--- c++/triqs/stat/histograms.cpp
+++ c++/triqs/stat/histograms.cpp
@@ -18,67 +18,69 @@
 // Authors: Philipp D, Igor Krivenko, Nils Wentzell
 
 #include "./histograms.hpp"
+#include "../utility/exceptions.hpp"
+
+#include <h5/h5.hpp>
+#include <nda/h5.hpp>
+
+#include <cmath>
+#include <ostream>
+#include <string>
 
 namespace triqs::stat {
 
-  void histogram::_init() {
-    if (a >= b) TRIQS_RUNTIME_ERROR << "histogram construction: one must have a<b";
-    _step = (n_bins - 1) / (b - a);
+  void histogram::initialize() {
+    if (a_ >= b_) TRIQS_RUNTIME_ERROR << "Error in histogram::initialize: Incorrect interval: a >= b";
+    if (size() < 2) TRIQS_RUNTIME_ERROR << "Error in histogram::initialize: Number of bins has to be at least 2";
+    binsize_ = (b_ - a_) / static_cast<double>(size() - 1);
   }
 
-  bool histogram::operator==(histogram const &h) const {
-    return this->a == h.a &&                 //
-       this->b == h.b &&                     //
-       this->n_bins == h.n_bins &&           //
-       this->_data == h._data &&             //
-       this->_n_data_pts == h._n_data_pts && //
-       this->_n_lost_pts == h._n_lost_pts && //
-       this->_step == h._step;
-  }
+  histogram::histogram(int a, int b) : a_(a), b_(b), data_(nda::vector<double>::zeros(b - a + 1)) { initialize(); }
+
+  histogram::histogram(double a, double b, std::size_t nbins) : a_(a), b_(b), data_(nbins) { initialize(); }
 
   histogram &histogram::operator<<(double x) {
-    if ((x < a) || (x > b))
-      ++_n_lost_pts;
+    if (x < a_ || x > b_)
+      ++n_lost_pts_;
     else {
-      auto n = int(std::floor(((x - a) * _step) + 0.5));
-      ++_data[n];
-      ++_n_data_pts;
+      auto n = static_cast<int>(std::floor(((x - a_) / binsize_) + 0.5));
+      ++data_[n];
+      ++n_data_pts_;
     }
     return *this;
   }
 
   histogram operator+(histogram h1, histogram const &h2) {
-    auto l1 = h1.limits(), l2 = h2.limits();
-    if (l1 != l2 || h1.size() != h2.size()) {
-      TRIQS_RUNTIME_ERROR << "Histograms with different limits in addition: [" << l1.first << ";" << l1.second << "] vs [" << l2.first << ";"
-                          << l2.second << "]";
-    }
-    h1._data += h2._data;
-    h1._n_data_pts += h2._n_data_pts;
-    h1._n_lost_pts += h2._n_lost_pts;
+    if (h1.limits() != h2.limits() || h1.size() != h2.size())
+      TRIQS_RUNTIME_ERROR << "Error when adding histograms: Histograms have different domains or number of bins";
+    h1.data_ += h2.data_;
+    h1.n_data_pts_ += h2.n_data_pts_;
+    h1.n_lost_pts_ += h2.n_lost_pts_;
     return h1;
   }
 
+  bool histogram::operator==(histogram const &h) const {
+    return a_ == h.a_ && b_ == h.b_ && data_ == h.data_ && n_data_pts_ == h.n_data_pts_ && n_lost_pts_ == h.n_lost_pts_ && binsize_ == h.binsize_;
+  }
+
   void h5_write(h5::group g, std::string const &name, histogram const &h) {
-    h5_write(g, name, h._data);
+    h5::write(g, name, h.data_);
     auto ds = g.open_dataset(name);
     write_hdf5_format(ds, h);
-    h5::write_attribute(ds, "a", h.a);
-    h5::write_attribute(ds, "b", h.b);
-    h5::write_attribute(ds, "n_bins", h.n_bins);
-    h5::write_attribute(ds, "n_data_pts", h._n_data_pts);
-    h5::write_attribute(ds, "n_lost_pts", h._n_lost_pts);
+    h5::write_attribute(ds, "a", h.a_);
+    h5::write_attribute(ds, "b", h.b_);
+    h5::write_attribute(ds, "n_data_pts", h.n_data_pts_);
+    h5::write_attribute(ds, "n_lost_pts", h.n_lost_pts_);
   }
 
   void h5_read(h5::group g, std::string const &name, histogram &h) {
-    h5_read(g, name, h._data);
+    h5::read(g, name, h.data_);
     auto ds = g.open_dataset(name);
-    h5::read_attribute(ds, "a", h.a);
-    h5::read_attribute(ds, "b", h.b);
-    h5::read_attribute(ds, "n_bins", h.n_bins);
-    h5::read_attribute(ds, "n_data_pts", h._n_data_pts);
-    h5::read_attribute(ds, "n_lost_pts", h._n_lost_pts);
-    h._init();
+    h5::read_attribute(ds, "a", h.a_);
+    h5::read_attribute(ds, "b", h.b_);
+    h5::read_attribute(ds, "n_data_pts", h.n_data_pts_);
+    h5::read_attribute(ds, "n_lost_pts", h.n_lost_pts_);
+    h.initialize();
   }
 
   std::ostream &operator<<(std::ostream &os, histogram const &h) {
--- test/c++/stat/stat_histograms_mpi.cpp
+++ test/c++/stat/stat_histograms_mpi.cpp
@@ -17,66 +17,63 @@
 //
 // Authors: Philipp D, Igor Krivenko, Nils Wentzell
 
-#include <triqs/test_tools/arrays.hpp>
 #include <triqs/stat/histograms.hpp>
+#include <triqs/test_tools/arrays.hpp>
 
-using namespace triqs::stat;
-using namespace triqs;
+#include <mpi/mpi.hpp>
 
-histogram make_hd() {
-  histogram hd{0, 10, 11};
+#include <vector>
+#include <utility>
+
+// Create histogram.
+auto make_histogram() {
+  triqs::stat::histogram h{0, 10, 11};
   std::vector<double> data{1.1, 2.0, 2.2, 2.9, 3.4, 5, 9, 10.0, 10.1, 12.1, 32.2};
-  for (auto i : data) hd << i;
-  return hd;
+  for (auto i : data) h << i;
+  return h;
 }
 
-TEST(histogram, mpi_broadcast) {
-
+TEST(TRIQSStat, HistogramMPIBroadcast) {
   mpi::communicator world;
-  auto h = world.rank() ? histogram{0, 20} : make_hd();
+  nda::vector<double> exp_h = {0, 1, 2, 2, 0, 1, 0, 0, 0, 1, 1};
 
-  nda::vector<double> true_h = {0, 1, 2, 2, 0, 1, 0, 0, 0, 1, 1};
+  // create and broadcast histogram
+  auto h = world.rank() ? triqs::stat::histogram{0, 20} : make_histogram();
   mpi::broadcast(h, world, 0);
-
   EXPECT_EQ(11, h.size());
   EXPECT_EQ(0, h.mesh_point(0));
   EXPECT_EQ(1, h.mesh_point(1));
   EXPECT_EQ(std::make_pair(.0, 10.), h.limits());
-  EXPECT_ARRAY_NEAR(true_h, h.data());
+  EXPECT_ARRAY_NEAR(exp_h, h.data());
   EXPECT_EQ(8, h.n_data_pts());
   EXPECT_EQ(3, h.n_lost_pts());
 }
 
-TEST(histogram, mpi_reduce) {
-
+TEST(TRIQSStat, HistogramMPIReduce) {
   mpi::communicator world;
-  auto h                         = make_hd();
-  nda::vector<double> true_h_rec = {0, 1, 2, 2, 0, 1, 0, 0, 0, 1, 1};
-  true_h_rec *= world.size();
+  nda::vector<double> exp_h = {0, 1, 2, 2, 0, 1, 0, 0, 0, 1, 1};
+  exp_h *= world.size();
 
-  // reduce
-
+  // create and reduce histogram
+  auto h  = make_histogram();
   auto h2 = mpi::reduce(h, world, 0);
-
   if (world.rank() == 0) {
     EXPECT_EQ(11, h2.size());
     EXPECT_EQ(0, h2.mesh_point(0));
     EXPECT_EQ(1, h2.mesh_point(1));
     EXPECT_EQ(std::make_pair(.0, 10.), h2.limits());
-    EXPECT_ARRAY_NEAR(true_h_rec, h2.data());
+    EXPECT_ARRAY_NEAR(exp_h, h2.data());
     EXPECT_EQ(8 * world.size(), h2.n_data_pts());
     EXPECT_EQ(3 * world.size(), h2.n_lost_pts());
   }
 
-  // all_reduce
-
+  // all_reduce histogram
   auto h3 = mpi::all_reduce(h, world);
-
   EXPECT_EQ(11, h3.size());
   EXPECT_EQ(0, h3.mesh_point(0));
   EXPECT_EQ(1, h3.mesh_point(1));
   EXPECT_EQ(std::make_pair(.0, 10.), h3.limits());
-  EXPECT_ARRAY_NEAR(true_h_rec, h3.data());
+  EXPECT_ARRAY_NEAR(exp_h, h3.data());
   EXPECT_EQ(8 * world.size(), h3.n_data_pts());
   EXPECT_EQ(3 * world.size(), h3.n_lost_pts());
 }
