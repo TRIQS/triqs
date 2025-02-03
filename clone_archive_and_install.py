f9dd4c72ded265601f884227054a2b2d23e836d7
Thomas Hahn <thomas.hahn01@gmail.com>
2025-02-03 15:37:57 -0500

    Simplify the offline installation script

--- clone_archive_and_install.py
+++ clone_archive_and_install.py
@@ -10,189 +10,135 @@ import tarfile
 from pathlib import Path
 
 
-# prepare repository information (the user can add/remove repositories as needed)
+# prepare repository information for the TRIQS core libraries (including triqs itself) and the applications based on
+# triqs (the user can add/remove repositories as needed)
 #
 # the following keys are supported:
-# - priority: lower number means less dependencies on other repos
 # - url: URL of the repository for cloning
 # - clone_args: arguments passed to the git clone command, e.g. "-b unstable"
 # - deps: list of repositories that should be soft linked to the deps folder
 # - cmake_args: additional arguments passed to the cmake command
 # - no_tests: do not run tests after building
-repos = {
+core_repos = {
     "GTest": {
-        "priority": 0,
         "url": "https://github.com/google/googletest",
         "clone_args": "-b main --depth 1"
     },
     "fmt": {
-        "priority": 0,
         "url": "https://github.com/fmtlib/fmt",
         "clone_args": "-b 10.2.1 --depth 1"
     },
     "Cpp2Py": {
-        "priority": 0,
         "url": "https://github.com/TRIQS/cpp2py.git",
         "clone_args": "-b unstable --depth 1"
     },
     "itertools": {
-        "priority": 1,
         "url": "https://github.com/TRIQS/itertools.git",
         "deps": ["GTest"]
     },
     "h5": {
-        "priority": 1,
         "url": "https://github.com/TRIQS/h5.git",
         "deps": ["GTest", "Cpp2Py"]
     },
     "mpi": {
-        "priority": 2,
         "url": "https://github.com/TRIQS/mpi.git",
         "deps": ["GTest", "itertools"]
     },
     "nda": {
-        "priority": 3,
         "url": "https://github.com/TRIQS/nda.git",
         "deps": ["GTest", "Cpp2Py", "itertools", "mpi", "h5"]
     },
     "cppdlr": {
-        "priority": 4,
         "url": "https://github.com/flatironinstitute/cppdlr.git",
         "deps": ["GTest", "nda", "fmt"]
     },
     "triqs": {
-        "priority": 5,
         "url": "https://github.com/TRIQS/triqs.git",
         "deps": ["GTest", "Cpp2Py", "itertools", "mpi", "h5", "nda", "fmt", "cppdlr"]
-    },
+    }
+}
+
+
+app_repos = {
     "cthyb": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/cthyb.git",
         "deps": ["GTest", "Cpp2Py"]
     },
     "dft_tools": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/dft_tools.git",
         "deps": ["GTest", "Cpp2Py"]
     },
     "tprf": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/tprf.git",
         "deps": ["GTest", "Cpp2Py"]
     },
     "maxent": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/maxent.git"
     },
     "hubbardI": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/hubbardI.git",
         "deps": ["GTest", "Cpp2Py"]
     },
     "hartree_fock": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/hartree_fock.git"
     },
     "solid_dmft": {
-        "priority": 6,
         "url": "https://github.com/TRIQS/solid_dmft.git"
     }
 }
 
 
-def parse_args():
+# merge the core_repos and app_repos dictionaries
+all_repos = {**core_repos, **app_repos}
+
+
+def clone_repo(name):
     """
-    Parse command-line arguments for the TRIQS (offline) installation script.
-
-    Returns
-    -------
-    argparse.Namespace
-        The parsed arguments as a namespace object.
-    """
-
-    parser = argparse.ArgumentParser(description="Clone, archive and install TRIQS related repositories", allow_abbrev=False)
-    parser.add_argument("-d", "--dir", default="triqs_repositories", help="directory containing (the cloned) repositories (default: ./triqs_repositories)")
-    parser.add_argument("-a", "--archive", action="store_true", help="archive the repository directory DIR (default: False)")
-    parser.add_argument("-c", "--clone", nargs="*", help="clone all or only specific repositories into DIR")
-    parser.add_argument("--clone-args", default="--depth 1", help="default arguments to pass to the git clone command (default: '--depth 1')")
-    parser.add_argument("-i", "--install", nargs="*", help="build and install all or only specific repositories")
-    parser.add_argument("--install-prefix", help="CMake install prefix (default: DIR/install)")
-    parser.add_argument("--cmake-args", default="", help="additional arguments to pass to the cmake command (default: '')")
-    parser.add_argument("--no-tests", action="store_true", help="do not run tests after building (default: False)")
-    parser.add_argument("-j", "--ncores", default=1, type=int, help="number of cores to use for building and installing (default: 1)")
-    args = parser.parse_args()
-    return args
-
-
-def clone_repos(args, repos):
-    """
-    Clone the specified repositories into the specified directory.
+    Clone the given repository and its dependencies recursively and create soft links for the dependencies.
 
     Parameters
     ----------
-    args : argparse.Namespace
-        The parsed command-line arguments.
-    repos : dict
-        A dictionary containing the repository information.
+    name : str
+        Name of the repository to clone.
 
     Raises
     ------
     subprocess.CalledProcessError
         If the git clone command fails.
     ValueError
-        If a specified repository is not found in the repository information.
+        If a specified repository is not found in the repository dictionary.
     """
 
-    print('=' * 25)
+    print('-' * 25)
+    print(f"Cloning the repository {name}")
 
-    # check repos to clone
-    if not args.clone:
-        # choose all repos if none are specified
-        print(f"No repositories specified to clone --> cloning all repositories")
-        args.clone = [*repos]
+    # check if the repo is in the repo dictionary
+    if name not in all_repos:
+        raise ValueError(f"Repository {name} not found in the repository dictionary.")
+
+    # get repo information from the dictionary
+    repo_info = all_repos[name]
+    url = repo_info["url"]
+    clone_args = repo_info.get("clone_args", args.clone_args)
+    repo_dir = args.dir + "/" + name + ".src"
+    repo_path = Path(repo_dir)
+
+    # clone the repo if hasn't been cloned yet
+    if not repo_path.exists():
+        subprocess.run(["git", "clone"] + clone_args.split() + [url, repo_dir], check=True)
     else:
-        print(f"User specified repositories to clone: {args.clone}")
+        print(f"Repository {name} already exists --> skipping")
 
-        # remove duplicate repos
-        args.clone = list(set(args.clone))
-
-         # loop over remaining repos
-        for name in args.clone:
-            # check if the repo is in the repo information
-            if name not in repos:
-                raise ValueError(f"Repository {name} not found in the repository information.")
-
-            # add missing dependencies
-            for dep_name in repos[name].get("deps", []):
-                if dep_name not in args.clone:
-                    print(f"Adding missing {name} dependency: {dep_name}")
-                    args.clone.append(dep_name)
-
-    # sort repos by priority
-    args.clone.sort(key=lambda x: repos[x]["priority"])
-
-    # output repos to clone
-    print(f"Cloning the following repositories: {args.clone}")
-    print('=' * 25)
-
-    # loop over repos to clone
-    for name in args.clone:
-        # get repo information
-        repo_info = repos[name]
-        url = repo_info["url"]
-        clone_args = repo_info.get("clone_args", args.clone_args)
-        repo_dir = args.dir + "/" + name + ".src"
-        repo_path = Path(repo_dir)
-
-        # clone the repo if hasn't been cloned yet
-        if not repo_path.exists():
-            subprocess.run(["git", "clone"] + clone_args.split() + [url, repo_dir], check=True)
-        else:
-            print(f"Repository {name} already exists --> skipping")
-
-        # create soft links for dependencies
+    # clone its dependencies recursively and create soft links
+    deps = repo_info.get("deps", [])
+    if deps:
+        print(f"Cloning the dependencies of {name}: {deps}")
         deps_path = repo_path / "deps"
-        for dep_name in repo_info.get("deps", []):
+        for dep_name in deps:
+            # clone the dependency
+            clone_repo(dep_name)
+
+            # create a soft link in the deps folder
             dst = deps_path / dep_name
             src = Path(args.dir + "/" + dep_name + ".src")
             if not dst.is_symlink() and src.exists():
@@ -200,24 +146,60 @@ def clone_repos(args, repos):
                 os.symlink(os.path.relpath(src, deps_path), dst)
             else:
                 print(f"Soft link {dep_name} for {name} already exists --> skipping")
-        print('-' * 25)
+    print('-' * 25)
 
 
-def create_archive(args):
-    """
-    Archives the specified directory into a tar.gz file.
+# epilog for the help message
+epilog = f"""
+repositories:
+    core: {', '.join(core_repos.keys())}
+    apps: {', '.join(app_repos.keys())}
 
-    Parameters
-    ----------
-    args : argparse.Namespace
-        The parsed command-line arguments.
+examples:
+    - clone and archive all repositories (core + apps):
+        python {os.path.relpath(__file__)} -c -a
 
-    Raises
-    ------
-    FileNotFoundError
-        If the specified directory does not exist.
-    """
+    - build + install all repositories (building in parallel with 12 cores):
+        python {os.path.relpath(__file__)} -i -j 12
 
+    - clone and install only triqs and cthyb (building in parallel with 12 cores):
+        python {os.path.relpath(__file__)} -c triqs cthyb -i triqs cthyb -j 12
+"""
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description="Clone, archive and install TRIQS related repositories", epilog=epilog,
+                                 formatter_class=argparse.RawDescriptionHelpFormatter, allow_abbrev=False)
+parser.add_argument("-d", "--dir", default="triqs_repositories", help="directory containing (the cloned) repositories (default: ./triqs_repositories)")
+parser.add_argument("-a", "--archive", action="store_true", help="archive the repository directory DIR (default: False)")
+parser.add_argument("-c", "--clone", nargs="*", help="clone all or only specific repositories into DIR")
+parser.add_argument("--clone-args", default="--depth 1", help="default arguments to pass to the git clone command (default: '--depth 1')")
+parser.add_argument("-i", "--install", nargs="*", help="build and install all or only specific repositories")
+parser.add_argument("--install-prefix", help="CMake install prefix (default: DIR/install)")
+parser.add_argument("--cmake-args", default="", help="additional arguments to pass to the cmake command (default: '')")
+parser.add_argument("--no-tests", action="store_true", help="do not run tests after building (default: False)")
+parser.add_argument("-j", "--ncores", default=1, type=int, help="number of cores to use for building and installing (default: 1)")
+args = parser.parse_args()
+
+# clone repositories
+if args.clone is not None:
+    print('=' * 25)
+
+    # check repos to clone
+    if not args.clone:
+        # choose all repos if none are specified
+        print(f"No repositories specified to clone --> cloning all repositories")
+        args.clone = [*core_repos, *app_repos]
+
+    # output repos to clone
+    print(f"Cloning the following repositories: {args.clone}")
+    print('=' * 25)
+
+    # loop over repos to clone and clone its dependencies recursively
+    for name in args.clone:
+        clone_repo(name)
+
+# archive the repository directory
+if args.archive:
     print('=' * 25)
 
     # check if the specified directory exists
@@ -234,35 +216,15 @@ def create_archive(args):
     with tarfile.open(repo_dir.name + ".tar.gz", "w:gz") as tar:
         tar.add(repo_dir)
 
-
-def build_and_install(args, repos):
-    """
-    Build and install the specified repositories.
-
-    Parameters
-    ----------
-    args : argparse.Namespace
-        The parsed command-line arguments.
-    repos : dict
-        A dictionary containing the repository information.
-
-    Raises
-    ------
-    subprocess.CalledProcessError
-        If the cmake or make command fails.
-    FileNotFoundError
-        If the src directory does not exist.
-    ValueError
-        If a specified repository is not found in the repository information.
-    """
-
+# build and install repositories
+if args.install is not None:
     print('=' * 25)
 
     # check repos to install
     if not args.install:
         # if no repos are specified, install triqs and all apps (core libs are installed by triqs automatically)
         print(f"No repositories specified to install --> installing TRIQS and all apps (core libraries are installed by TRIQS automatically)")
-        args.install = [x[0] for x in repos.items() if x[1]["priority"] >= 5]
+        args.install = ['triqs', *app_repos]
     else:
         print(f"User specified repositories to install: {args.install}")
 
@@ -272,17 +234,17 @@ def build_and_install(args, repos):
         # loop over remaining repos
         for name in args.install:
             # check if the repo is in the repo information
-            if name not in repos:
+            if name not in all_repos:
                 raise ValueError(f"Repository {name} not found in the repository information.")
 
             # filter out dependencies that will be installed by another repo with higher priority
-            for dep_name in repos[name].get("deps", []):
+            for dep_name in all_repos[name].get("deps", []):
                 if dep_name in args.install:
                     print(f"Repository {dep_name} will be installed by {name} automatically --> removing")
                     args.install = [x for x in args.install if x != dep_name]
 
-    # sort repos by priority
-    args.install.sort(key=lambda x: repos[x]["priority"])
+        # sort repos according to the order in the all_repos dictionary
+        args.install.sort(key=lambda x: list(all_repos.keys()).index(x))
 
     # output repositories to install
     print(f"Installing the following repositories: {args.install}")
@@ -293,20 +255,20 @@ def build_and_install(args, repos):
         print(f"Building and installing {name}")
 
         # get repo information
-        repo_info = repos[name]
+        repo_info = all_repos[name]
 
         # check if the src directory exists
         repo_path = Path(args.dir + "/" + name + ".src")
         if not repo_path.exists():
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), repo_path.name)
 
-        # build and install directory
+        # set up build and install directory
         build_dir = Path(args.dir + "/" + name + ".build")
         install_prefix = Path(args.dir + "/install" if args.install_prefix is None else args.install_prefix)
         cmake_install_prefix = "-DCMAKE_INSTALL_PREFIX=" + str(install_prefix.absolute())
 
-        # source triqs shell variables for apps
-        if repo_info["priority"] >= 6 and "TRIQS_ROOT" not in os.environ:
+        # source triqs shell variables for apps if not already in the environment
+        if name in app_repos and "TRIQS_ROOT" not in os.environ:
             py_version = str(sys.version_info.major) + "." + str(sys.version_info.minor)
             py_path = str((install_prefix / ("lib/python" + py_version + "/site-packages")).absolute())
             os.environ["TRIQS_ROOT"] = str(install_prefix.absolute())
@@ -323,19 +285,3 @@ def build_and_install(args, repos):
         subprocess.run(["cmake", "--install", str(build_dir)], check=True)
 
         print('-' * 25)
-
-
-# command line arguments
-args = parse_args()
-
-# clone repositories
-if args.clone is not None:
-    clone_repos(args, repos)
-
-# archive the repository directory
-if args.archive:
-    create_archive(args)
-
-# build and install repositories
-if args.install is not None:
-    build_and_install(args, repos)
