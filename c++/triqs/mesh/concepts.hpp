89e0ba3e54568dde2cc7d20ff95309f860dc6324
Thomas Hahn <thomas.hahn01@gmail.com>
2025-06-16 17:03:01 -0400

    Add docs to mesh/concepts.hpp and do minor clean up
    - fix includes
    - remove unused Hashable concept
    - remove closest_mesh_point_t forward declaration

--- c++/triqs/mesh/concepts.hpp
+++ c++/triqs/mesh/concepts.hpp
@@ -15,14 +15,21 @@
 //
 // Authors: Olivier Parcollet, Nils Wentzell
 
-#pragma once
+/**
+ * @file
+ * @brief Provides concepts for mesh points and meshes.
+ */
 
-#include <concepts>
-#include <ranges>
-#include <string>
+#pragma once
 
 #include <h5/h5.hpp>
 #include <nda/concepts.hpp>
+
+#include <cstdint>
+#include <concepts>
+#include <cstdint>
+#include <ranges>
+#include <string>
 
 namespace triqs {
   using nda::AnyOf;
@@ -30,87 +37,112 @@ namespace triqs {
 
 namespace triqs::mesh {
 
-  /// The type is hashable with std::hash
-  template <typename T>
-  concept Hashable = requires(T a) {
-    { std::hash<T>{}(a) } -> std::convertible_to<std::size_t>;
-  };
+  /**
+   * @addtogroup triqs-meshes-concepts
+   * @{
+   */
 
-  // ----------- MeshPoint  ------------------------
-
+  /**
+   * @brief Concept for a mesh point.
+   *
+   * @details Every mesh point `mp` of type `MP`
+   * - belongs to a mesh `MP::mesh_t`,
+   * - has a unique index `mp.index()` that identifies a mesh point within the mesh,
+   * - has a data index `mp.data_index()` that is used to access function values/coefficients stored in GF containers 
+   * and
+   * - has a hash value `mp.mesh_hash()` that is used to check if a mesh point belongs to or is compatible with a mesh.
+   *
+   * @tparam MP Mesh point type.
+   */
   template <typename MP>
   concept MeshPoint = requires(MP const &mp) {
-    // The parent mesh type
+    // parent mesh type
     typename MP::mesh_t;
 
-    // The associated index, e.g. Matsubara Index or a long
-    { mp.index() } -> AnyOf<typename MP::mesh_t::index_t, typename MP::mesh_t::index_t const &>;
+    // unique index, e.g. Matsubara index, a simple long or a std::array<long, 3> for k-points
+    { mp.index() } -> nda::AnyOf<typename MP::mesh_t::index_t, typename MP::mesh_t::index_t const &>;
 
-    // The index into the data array
-    { mp.data_index() } -> AnyOf<typename MP::mesh_t::data_index_t, typename MP::mesh_t::data_index_t const &>;
+    // the data index to access data arrays of GF containers
+    { mp.data_index() } -> nda::AnyOf<typename MP::mesh_t::data_index_t, typename MP::mesh_t::data_index_t const &>;
 
-    // Hash for easy checking of MeshPoint and Mesh compatibility
+    // hash of the parent mesh to check for compatibility
     { mp.mesh_hash() } -> std::same_as<uint64_t>;
   };
 
-  // ----------- Mesh ------------------------
-
-  /// A mesh is a range of mesh points that can optionally have values, e.g. in [0,beta].
-  /// Each mesh point holds an index (e.g. Matsubara index) and a data index for
-  /// corresponding to the Green function data array in memory
+  /**
+   * @brief Concept for a mesh.
+   *
+   * @details Every mesh `m` of type `M`
+   * - is a `std::regular` type and `h5::Storable`,
+   * - has an associated triqs::mesh::MeshPoint type `M::mesh_point_t`, index type `M::index_t` and data index type
+   * `M::data_index_t`,
+   * - is a sized, forward range of mesh points,
+   * - provides an overload for the subscript operator to access a certain mesh point by data index,
+   * - provides an overload for the function call operator to access a certain mesh point by index,
+   * - provides the functions `m.to_data_index(index)` and `m.to_index(data_index)` to convert between indices and data
+   * indices and
+   * - provides the function `m.is_index_valid(index)` to check if an index is valid.
+   *
+   * @tparam M Mesh type.
+   */
   template <typename M>
   concept Mesh = std::regular<M> and h5::Storable<M> and requires(M const &m) {
-
-    // Mesh has a mesh point
+    // mesh point type
     typename M::mesh_point_t;
     requires MeshPoint<typename M::mesh_point_t>;
 
-    // Mesh is a range of mesh point
+    // sized, forward range of mesh points
 #if not(defined(_LIBCPP_VERSION) and (__clang_major__ < 16))
-    // Do NOT check on old compiler ....
     requires std::ranges::forward_range<M>;
     requires std::ranges::sized_range<M>;
 #endif
     { *std::begin(m) } -> std::same_as<typename M::mesh_point_t>;
 
-    // The type of the index, e.g. Matsubara Index
+    // index type
     typename M::index_t;
 
-    // The type of the index indexing the data array
+    // data index type
     typename M::data_index_t;
   }
-  // we now check some operations combining indices, mesh, mesh_points
   and requires(M const &m, typename M::index_t index, typename M::data_index_t data_index, typename M::mesh_point_t mp) {
-    // Validity of the index
+    // check validity of an index
     { m.is_index_valid(index) } -> std::same_as<bool>;
 
-    // Conversion
-    // NB the data index-> index could be slow (unflatten) but is necessary in some cases.
-    { m.to_data_index(index) } -> std::same_as<typename M::data_index_t>;         // index -> data idx
-    { m.to_index(data_index) } -> std::same_as<typename M::index_t>;            // data idx -> idx
-    { m.operator[](data_index) } -> std::same_as<typename M::mesh_point_t>; // data index -> mesh point
-    { m.operator()(index) } -> std::same_as<typename M::mesh_point_t>;    // index -> mesh point
+    // index <-> data index conversion
+    { m.to_data_index(index) } -> std::same_as<typename M::data_index_t>;
+    { m.to_index(data_index) } -> std::same_as<typename M::index_t>;
+
+    // access mesh points
+    { m.operator[](data_index) } -> std::same_as<typename M::mesh_point_t>;
+    { m.operator()(index) } -> std::same_as<typename M::mesh_point_t>;
 
     // Hash for easy checking of MeshPoint and Mesh compatibility
     { m.mesh_hash() } -> std::same_as<uint64_t>;
   };
 
-  // ------------- MeshWithValues -----------------
-
-  template <typename T> struct closest_mesh_point_t; // Forward declaration
-
-  // Some meshes have points that can take value in a domain,
-  // e.g. a k mesh in a Brillouin zone, k can be a R3 vector.
+  /**
+   * @brief Concept for a mesh with values.
+   *
+   * @details Every mesh with values `m` of type `M`
+   * - is a triqs::mesh::Mesh,
+   * - has mesh points which have a value of type `M::value_t` and which can be cast to this type and
+   * - provides a function `m.to_value(index)` to convert an index of a mesh point to its value.
+   *
+   * @tparam M Mesh type.
+   */
   template <typename M>
   concept MeshWithValues = Mesh<M> and requires(M const &m, typename M::index_t index) {
-    // The value type of the domain
+    // value type
     typename M::value_t;
 
-    { m.to_value(index) } -> std::same_as<typename M::value_t>; // index -> value
+    // convert an index to its value
+    { m.to_value(index) } -> std::same_as<typename M::value_t>;
 
-    // Mesh Points can return their value and are castable
-    { (*std::begin(m)).value() } -> AnyOf<typename M::value_t, typename M::value_t const &>;
+    // mesh points can return their value and are castable
+    { (*std::begin(m)).value() } -> nda::AnyOf<typename M::value_t, typename M::value_t const &>;
     { static_cast<typename M::value_t>(*std::begin(m)) };
   };
+
+  /** @} */
 
 } // namespace triqs::mesh
