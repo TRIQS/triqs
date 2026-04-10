5fe7d6a1fbca0e6cb8f95bfd14678b0442ee9ffc
Thomas Hahn <thomas.hahn01@gmail.com>
2026-04-10 10:59:18 -0400

    Add generated python bindings for random_generator module

---
+++ python/triqs/mc_tools/random_generator.wrap.hxx
@@ -0,0 +1,7 @@
+#include <c2py/c2py.hpp>
+
+#ifndef C2PY_HXX_DECLARATION_random_generator_GUARDS
+#define C2PY_HXX_DECLARATION_random_generator_GUARDS
+template <> constexpr bool c2py::is_wrapped<triqs::mc_tools::random_generator>     = true;
+template <> inline constexpr auto c2py::tp_name<triqs::mc_tools::random_generator> = "triqs.mc_tools.random_generator.RandomGenerator";
+#endif
