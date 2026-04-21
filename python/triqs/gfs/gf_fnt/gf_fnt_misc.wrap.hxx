fac835206482746e7e766c1b1aeaf09bd803422f
Nils Wentzell <nwentzell@flatironinstitute.org>
2026-04-21 15:58:27 -0400

    Remove empty hxx files

--- python/triqs/gfs/gf_fnt/gf_fnt_misc.wrap.hxx
+++
@@ -1,6 +0,0 @@
-#include <c2py/c2py.hpp>
-
-#ifndef C2PY_HXX_DECLARATION_gf_fnt_misc_GUARDS
-#define C2PY_HXX_DECLARATION_gf_fnt_misc_GUARDS
-
-#endif
--- python/triqs/gfs/wrapped_aux/wrapped_aux_other.wrap.hxx
+++
@@ -1,6 +0,0 @@
-#include <c2py/c2py.hpp>
-
-#ifndef C2PY_HXX_DECLARATION_wrapped_aux_other_GUARDS
-#define C2PY_HXX_DECLARATION_wrapped_aux_other_GUARDS
-
-#endif
