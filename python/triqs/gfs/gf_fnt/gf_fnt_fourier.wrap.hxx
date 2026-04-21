90a2611410f75ec15a9e82aaa0657acbfd429103
Nils Wentzell <nwentzell@flatironinstitute.org>
2026-04-21 15:58:27 -0400

    Remove empty hxx files

--- python/triqs/gfs/gf_fnt/gf_fnt_fourier.wrap.hxx
+++
@@ -1,6 +0,0 @@
-#include <c2py/c2py.hpp>
-
-#ifndef C2PY_HXX_DECLARATION_gf_fnt_fourier_GUARDS
-#define C2PY_HXX_DECLARATION_gf_fnt_fourier_GUARDS
-
-#endif
--- python/triqs/operators/util/extractors.wrap.hxx
+++
@@ -1,6 +0,0 @@
-#include <c2py/c2py.hpp>
-
-#ifndef C2PY_HXX_DECLARATION_extractors_GUARDS
-#define C2PY_HXX_DECLARATION_extractors_GUARDS
-
-#endif
