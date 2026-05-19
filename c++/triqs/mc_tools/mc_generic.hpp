8ec194b76f1e656151fd9d98a52d4d71c41d233a
Nils Wentzell <nwentzell@flatironinstitute.org>
2026-05-19 14:52:59 -0400

    [mc_generic] Fix issue of trying to call uninitialized pointer to mpi::monitor
    Back-port of 62450179, adapted to 3.3.x mpi::monitor API
    (uses request_emergency_stop, not report_local_event).

--- c++/triqs/mc_tools/mc_generic.hpp
+++ c++/triqs/mc_tools/mc_generic.hpp
@@ -286,10 +286,14 @@ namespace triqs::mc_tools {
         } catch (std::exception const &err) {
           // log the error and node number
           std::cerr << "mc_generic: Exception occurs on node " << c.rank() << "\n" << err.what() << std::endl;
-          if (rethrow_exception)
-            node_monitor->request_emergency_stop();
-          else
-            c.abort(2);
+          if (mpi::has_env) {
+            if (rethrow_exception)
+              node_monitor->request_emergency_stop();
+            else
+              c.abort(2);
+          } else {
+            throw;
+          }
         }
 
         // recompute fraction done
