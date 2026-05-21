d7ea5200a3f3a11a5d6f5141825a964acf69d281
Thomas Hahn <thomas.hahn01@gmail.com>
2026-05-21 19:14:42 -0400

    Update lattice/grid_generator.hpp
    - add doxygen docs
    - fix includes
    - simplify

--- c++/triqs/lattice/grid_generator.hpp
+++ c++/triqs/lattice/grid_generator.hpp
@@ -17,77 +17,123 @@
 //
 // Authors: Michel Ferrero, Olivier Parcollet, Nils Wentzell
 
+/**
+ * @file
+ * @brief Provides a forward iterator generating a regular grid of points in a cuboid.
+ */
+
 #pragma once
-#include "./brillouin_zone.hpp"
+
 #include <itertools/itertools.hpp>
+#include <nda/nda.hpp>
 
-namespace triqs {
-  namespace lattice {
+#include <iterator>
 
-    using v_t = nda::vector<double>;
+namespace triqs::lattice {
 
+  /**
+   * @addtogroup triqs-lattice
+   * @{
+   */
+
+  /// Point type used by grid_generator: a real-valued vector (`nda::vector<double>`).
+  using v_t = nda::vector<double>;
+
+  /**
+   * @brief Forward iterator generating uniformly-spaced points in the d-dimensional unit cube \f$ [0,1)^d \f$.
+   *
+   * @details For a given dimension \f$ d \in \{1,2,3\} \f$ and a number of points \f$ n \f$ per dimension, the 
+   * generator iterates over the cuboid grid of \f$ n^d \f$ points
+   * \f[
+   *   \mathbf{p}_{(i_1,\dots,i_d)} = \left( \frac{i_1 + 1/2}{n}, \dots, \frac{i_d + 1/2}{n} \right) , \quad i_k \in 
+   *   \{0,\dots,n-1\} \; ,
+   * \f]
+   * i.e. each point is shifted by half a step away from the cuboid faces so that the grid lies strictly inside the 
+   * open unit cube. The iteration order is x (fastest), then y, then z (slowest).
+   *
+   * The generated points are always 3-dimensional. For \f$ d < 3 \f$ the trailing coordinates are kept at their
+   * half-step value \f$ 1/2 \f$.
+   *
+   * Typical usage in C++ is via the implicit `bool` conversion and the increment operator inherited from
+   * `itertools::iterator_facade`:
+   * @code
+   * for (auto gen = grid_generator{3, 10}; gen; ++gen) {
+   *   nda::vector<double> const& k = *gen;
+   *   // ... use k ...
+   * }
+   * @endcode
+   */
+  class grid_generator : public itertools::iterator_facade<grid_generator, v_t const, std::forward_iterator_tag> {
+    friend struct itertools::iterator_facade<grid_generator, v_t const, std::forward_iterator_tag>;
+    int dim{3}, nkpts{0}, nx{0}, ny{0}, nz{0}, N_X{0}, N_Y{0}, N_Z{0}, index_{0};
+    double step_x{0.0}, step_y{0.0}, step_z{0.0};
+    bool at_end{false};
+    v_t pt;
+    void init() {
+      N_X    = nkpts;
+      N_Y    = (dim > 1 ? nkpts : 1);
+      N_Z    = (dim > 2 ? nkpts : 1);
+      step_x = 1.0 / double(N_X);
+      step_y = 1.0 / double(N_Y);
+      step_z = 1.0 / double(N_Z);
+      pt(0)  = step_x / 2;
+      pt(1)  = step_y / 2;
+      pt(2)  = step_z / 2;
+    }
+
+    void increment() {
+      if (nx < N_X - 1) {
+        ++nx;
+        pt(0) += step_x;
+        ++index_;
+        return;
+      }
+      pt(0) = step_x / 2;
+      nx    = 0;
+      if (ny < N_Y - 1) {
+        ++ny;
+        pt(1) += step_y;
+        ++index_;
+        return;
+      }
+      pt(1) = step_y / 2;
+      ny    = 0;
+      if (nz < N_Z - 1) {
+        ++nz;
+        pt(2) += step_z;
+        ++index_;
+        return;
+      }
+      at_end = true;
+    }
+
+    value_type dereference() const { return pt; }
+
+    public:
     /**
-  * Generate the point in a cuboid as an array<double,1> const &
-  */
-    class grid_generator : public itertools::iterator_facade<grid_generator, v_t const, std::forward_iterator_tag> {
-      friend struct itertools::iterator_facade<grid_generator, v_t const, std::forward_iterator_tag>;
-      int dim, nkpts, nx, ny, nz, N_X, N_Y, N_Z, index_;
-      double step_x, step_y, step_z;
-      bool at_end;
-      v_t pt;
-      void init() {
-        nx     = 0;
-        ny     = 0;
-        nz     = 0;
-        at_end = false;
-        index_ = 0;
-        N_X    = nkpts;
-        N_Y    = (dim > 1 ? nkpts : 1);
-        N_Z    = (dim > 2 ? nkpts : 1);
-        step_x = 1.0 / double(N_X);
-        step_y = 1.0 / double(N_Y);
-        step_z = 1.0 / double(N_Z);
-        pt(0)  = step_x / 2;
-        pt(1)  = step_y / 2;
-        pt(2)  = step_z / 2;
-      }
+     * @brief Construct a grid generator with the given dimension and number of points per dimension.
+     *
+     * @param dim Dimension of the grid (1, 2 or 3).
+     * @param nkpts Number of points along each dimension.
+     */
+    grid_generator(int dim, int nkpts) : dim(dim), nkpts(nkpts), pt(3) { init(); }
 
-      void increment() {
-        if (nx < N_X - 1) {
-          ++nx;
-          pt(0) += step_x;
-          ++index_;
-          return;
-        }
-        pt(0) = step_x / 2;
-        nx    = 0;
-        if (ny < N_Y - 1) {
-          ++ny;
-          pt(1) += step_y;
-          ++index_;
-          return;
-        }
-        pt(1) = step_y / 2;
-        ny    = 0;
-        if (nz < N_Z - 1) {
-          ++nz;
-          pt(2) += step_z;
-          ++index_;
-          return;
-        }
-        at_end = true;
-      }
+    /// Default constructor: produces a 3-dimensional generator with zero points (an empty/end iterator).
+    grid_generator() : pt(3) { init(); }
 
-      value_type dereference() const { return pt; }
+    /// Equal-to comparison: two generators are equal if they share the same dimension, number of points and index.
+    bool operator==(grid_generator const &other) const { return ((other.dim == dim) && (other.index_ == index_) && (other.nkpts == nkpts)); }
 
-      public:
-      /// dim : dimension, nkpts : number of k point in each dimension
-      grid_generator(int dim_, int nkpts_) : dim(dim_), nkpts(nkpts_), pt(3) { init(); }
-      grid_generator() : dim(3), nkpts(0), pt(3) { init(); }
-      bool operator==(grid_generator const &other) const { return ((other.dim == dim) && (other.index_ == index_) && (other.nkpts == nkpts)); }
-      int size() const { return (N_X * N_Y * N_Z); }
-      int index() const { return index_; }
-      operator bool() const { return !(at_end); }
-    };
-  } // namespace lattice
-} // namespace triqs
+    /// Total number of grid points, equal to \f$ N_X \cdot N_Y \cdot N_Z \f$ (trailing factors are 1 for `dim < 3`).
+    int size() const { return (N_X * N_Y * N_Z); }
+
+    /// Linear index of the current grid point in the iteration order (x fastest, then y, then z).
+    int index() const { return index_; }
+
+    /// Implicit conversion to `bool`: true while the generator has not yet been exhausted.
+    operator bool() const { return !(at_end); }
+  };
+
+  /** @} */
+
+} // namespace triqs::lattice
