de77557506f74fef529864bc0c87c39588a9d7a0
Nils Wentzell <nwentzell@flatironinstitute.org>
2026-05-27 10:05:01 -0400

    Restore non-symmetric fit_gf_dlr in gf_dlr Multivar test

--- test/c++/gfs/gf_dlr.cpp
+++ test/c++/gfs/gf_dlr.cpp
@@ -422,7 +422,7 @@ TEST(Gf, DLR_multivar_fit) {
   for (double sigma : {0.1, 0.01, 0.001, 0.0001, 0.00001}) {
     auto gtau_noise = gtau;
     gtau_noise.data() += sigma * (nda::rand(n_tau, n_tau) - 0.5);
-    auto gcoef_noise = fit_gf_dlr<0, 1>(gtau_noise, w_max, eps);
+    auto gcoef_noise = fit_gf_dlr<0, 1>(gtau_noise, w_max, eps, false);
     auto gtau3       = make_gf_imtime<0, 1>(gcoef_noise, n_tau);
     EXPECT_GF_NEAR(gtau, gtau3, 30 * sigma);
   }
